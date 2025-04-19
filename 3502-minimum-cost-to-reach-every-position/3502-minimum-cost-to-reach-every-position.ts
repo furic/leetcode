52377ddda27444143611bad64556258225beed6f
Richard Fu <44884321+furic@users.noreply.github.com>
2025-04-20 01:04:50 +1000

    Time: 0 ms (100%), Space: 59.1 MB (6.19%) - LeetHub

---
+++ 3502-minimum-cost-to-reach-every-position/3502-minimum-cost-to-reach-every-position.ts
@@ -0,0 +1,12 @@
+function minCosts(cost: number[]): number[] {
+    const n = cost.length;
+    const answer: number[] = new Array(n).fill(0);
+
+    answer[0] = cost[0];
+
+    for (let i = 1; i < n; i++) {
+        answer[i] = Math.min(answer[i - 1], cost[i]);
+    }
+
+    return answer;
+};

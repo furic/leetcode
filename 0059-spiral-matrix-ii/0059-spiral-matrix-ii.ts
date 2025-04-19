270b95e6704679889921cf74ea5cc44ce0a1eff8
Richard Fu <44884321+furic@users.noreply.github.com>
2025-04-20 01:14:39 +1000

    Time: 0 ms (100%), Space: 56.8 MB (36.44%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.ts
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.ts
@@ -1,4 +1,4 @@
-function generateMatrix(n: number): number[][] {
+const generateMatrix = (n: number): number[][] => {
     const result = Array.from({ length: n }, () => Array(n));
     let len = n, dir = 1, vDir = false, steps = n, x = -1, y = 0;
     for (let val = 1; val <= n * n; val++) {

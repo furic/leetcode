c42df8347b4ce5b2f5ff5f23263e49d2c828cdd3
Richard Fu <44884321+furic@users.noreply.github.com>
2025-04-20 01:13:35 +1000

    Time: 37 ms (14.29%), Space: 71.2 MB (14.29%) - LeetHub

---
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.ts
@@ -0,0 +1,24 @@
+function spiralMatrixIII(rows: number, cols: number, rStart: number, cStart: number): number[][] {
+    const visited = new Set<string>()
+    const result = [];
+
+    let dirY = 0;
+    let dirX = 1;
+    let y = rStart;
+    let x = cStart;
+    let count = 1;
+    while (count <= rows * cols) {
+        visited.add(`${y},${x}`);
+        if (0 <= y && y < rows && 0 <= x && x < cols) {
+            result.push([y, x]);
+            count += 1;
+        }
+        y += dirY;
+        x += dirX;
+        if (!visited.has(`${y + dirX},${x - dirY}`)) {
+            [dirX, dirY] = [-dirY, dirX];
+        }
+    }
+
+    return result;
+};

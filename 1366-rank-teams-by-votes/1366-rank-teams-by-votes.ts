94d2069d7d3e9bdfc6f54820f8f29b6acd43668d
Richard Fu <44884321+furic@users.noreply.github.com>
2025-04-20 01:15:29 +1000

    Time: 17 ms (82%), Space: 61.9 MB (62%) - LeetHub

---
+++ 1366-rank-teams-by-votes/1366-rank-teams-by-votes.ts
@@ -0,0 +1,28 @@
+function rankTeams(votes: string[]): string {
+    const teamCount = votes[0].length;
+    const teamVoteMap = new Map<string, number[]>(); // Key: team, value: array of vote count on each rank, e.g. [1st_count, 2nd_count, ...]
+
+    for (const team of votes[0]) {
+        teamVoteMap.set(team, Array(teamCount).fill(0));
+    }
+    
+    for (const vote of votes) {
+        for (let rank = 0; rank < teamCount; rank++) {
+            const team = vote[rank];
+            teamVoteMap.get(team)[rank]++;
+        }
+    }
+    
+    const sortedTeams = Array.from(teamVoteMap.keys()).sort((a, b) => {
+        const aRanks = teamVoteMap.get(a);
+        const bRanks = teamVoteMap.get(b);
+        for (let i = 0; i < teamCount; i++) {
+            if (aRanks[i] !== bRanks[i]) {
+                return bRanks[i] - aRanks[i];
+            }
+        }
+        return a.localeCompare(b);
+    });
+    
+    return sortedTeams.join('');
+};

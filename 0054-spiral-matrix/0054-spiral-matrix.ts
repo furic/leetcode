89f5fcc36f0f11e1543ba310a764a977657dffe7
Richard Fu <44884321+furic@users.noreply.github.com>
2025-04-20 01:15:01 +1000

    Time: 0 ms (100%), Space: 55.2 MB (66.55%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.ts
@@ -0,0 +1,12 @@
+function spiralOrder(matrix: number[][]): number[] {
+    const result: number[] = [];
+    while (matrix.length > 0 && matrix[0][0] !== undefined) {
+        // Steal the first row
+        result.push(...matrix.shift());
+        // Steal the right column
+        matrix.forEach(row => result.push(row.pop()));
+        // Turn the matrix over
+        matrix.reverse().map(row => row.reverse());
+    }
+    return result;
+};

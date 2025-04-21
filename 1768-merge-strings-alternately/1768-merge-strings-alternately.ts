f17df19560fd60a69770fa1fc467b33cea7a8f14
Richard Fu <44884321+furic@users.noreply.github.com>
2025-04-21 14:31:44 +1000

    Time: 48 ms (53.04%), Space: 58.3 MB (5.62%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.ts
@@ -0,0 +1,10 @@
+function mergeAlternately(word1: string, word2: string): string {
+    let result = '';
+    let current = word1;
+    let next = word2;
+    while (current.length > 0 && next.length > 0) {
+        result += current[0];
+        [current, next] = [next, current.slice(1)];
+    }
+    return result + current + next;
+};

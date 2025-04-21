61e4155a2392c3b5727daa1adbb80de51181e0aa
Richard Fu <44884321+furic@users.noreply.github.com>
2025-04-21 15:50:08 +1000

    Time: 1 ms (72.73%), Space: 53.5 MB (100%) - LeetHub

---
+++ 3423-maximum-difference-between-adjacent-elements-in-a-circular-array/3423-maximum-difference-between-adjacent-elements-in-a-circular-array.ts
@@ -0,0 +1,8 @@
+function maxAdjacentDistance(nums: number[]): number {
+    let maxDifference = 0;
+    for (let i = 0; i < nums.length; i++) {
+        const diff = Math.abs(nums[i] - nums[(i + 1) % nums.length]);
+        maxDifference = Math.max(maxDifference, diff);
+    }
+    return maxDifference;
+};

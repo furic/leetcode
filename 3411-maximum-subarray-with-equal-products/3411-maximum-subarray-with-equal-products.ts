66b149d4afe11d62ce2c404f6f2b9ba23e168840
Richard Fu <44884321+furic@users.noreply.github.com>
2025-04-21 15:54:43 +1000

    Time: 82 ms (26.67%), Space: 63 MB (40%) - LeetHub

--- 3411-maximum-subarray-with-equal-products/3411-maximum-subarray-with-equal-products.ts
+++ 3411-maximum-subarray-with-equal-products/3411-maximum-subarray-with-equal-products.ts
@@ -1,37 +1,37 @@
-function maxLength(nums: number[]): number {
+const maxLength = (nums: number[]): number => {
     let result = 0;
     for (let start = 0; start < nums.length; start++) {
         for (let end = start; end < nums.length; end++) {
             const arr = nums.slice(start, end + 1);
             if (prod(arr) === lcm(arr) * gcd(arr)) {
-                result = Math.max(result, arr.length)
+                result = Math.max(result, arr.length);
             }
         }
     }
     return result;
 };
 
-function prod(numbers: number[]): number {
+const prod = (numbers: number[]): number => {
     return numbers.reduce((acc, num) => acc * num, 1);
-}
+};
 
-function gcdTwoNumbers(a: number, b: number): number {
+const gcdTwoNumbers = (a: number, b: number): number => {
     while (b !== 0) {
         const temp = b;
         b = a % b;
         a = temp;
     }
     return a;
-}
+};
 
-function gcd(arr: number[]): number {
+const gcd = (arr: number[]): number => {
     return arr.reduce((acc, num) => gcdTwoNumbers(acc, num));
-}
+};
 
-function lcmTwoNumbers(a: number, b: number): number {
+const lcmTwoNumbers = (a: number, b: number): number => {
     return (a * b) / gcdTwoNumbers(a, b);
-}
+};
 
-function lcm(arr: number[]): number {
+const lcm = (arr: number[]): number => {
     return arr.reduce((acc, num) => lcmTwoNumbers(acc, num));
-}
+};

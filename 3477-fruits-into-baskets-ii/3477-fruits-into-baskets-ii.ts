7407e3ef80505e62094b87ce159bf3cec8f99760
Richard Fu <44884321+furic@users.noreply.github.com>
2025-04-21 15:45:44 +1000

    Time: 3 ms (49.51%), Space: 58.2 MB (55.34%) - LeetHub

---
+++ 3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.ts
@@ -0,0 +1,16 @@
+function numOfUnplacedFruits(fruits: number[], baskets: number[]): number {
+    let count = 0;
+    for (const fruit of fruits) {
+        for (let i = 0; i < baskets.length; i++) {
+            if (fruit <= baskets[i]) {
+                baskets[i] = 0;
+                break;
+            }
+            if (i === baskets.length - 1) { 
+                count++;
+            }
+        }
+        
+    }
+    return count;
+};

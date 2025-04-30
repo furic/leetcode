a8c9f6ba51933289ac5b8ef4bbef4e99ba7c6ee9
Richard Fu <44884321+furic@users.noreply.github.com>
2025-04-30 12:26:56 +1000

    Time: 24 ms (44.35%), Space: 101 MB (5.22%) - LeetHub

---
+++ 3355-zero-array-transformation-i/3355-zero-array-transformation-i.ts
@@ -0,0 +1,18 @@
+const isZeroArray = (nums: number[], queries: number[][]): boolean => {
+    const n = nums.length;
+    const decrementMarks = new Array(n + 1).fill(0);
+
+    for (const [start, end] of queries) {
+        decrementMarks[start]--;
+        if (end + 1 < n) decrementMarks[end + 1]++;
+    }
+
+    let appliedDecrement = 0;
+    for (let i = 0; i < n; i++) {
+        appliedDecrement += decrementMarks[i];
+        const finalValue = nums[i] + appliedDecrement;
+        if (finalValue > 0) return false;
+    }
+
+    return true;
+};

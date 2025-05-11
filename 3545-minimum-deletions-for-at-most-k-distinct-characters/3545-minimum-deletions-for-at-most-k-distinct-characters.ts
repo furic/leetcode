101774202bea3fcbeeba7381a44261048b83e34a
Richard Fu <44884321+furic@users.noreply.github.com>
2025-05-11 21:27:03 +1000

    Time: 1 ms (100%), Space: 58.1 MB (95.45%) - LeetHub

--- 3545-minimum-deletions-for-at-most-k-distinct-characters/3545-minimum-deletions-for-at-most-k-distinct-characters.ts
+++ 3545-minimum-deletions-for-at-most-k-distinct-characters/3545-minimum-deletions-for-at-most-k-distinct-characters.ts
@@ -1,8 +1,8 @@
 const minDeletion = (s: string, k: number): number => {
-    const counts = new Array(26).fill(0);
-    s.split("").map((c) => counts[c.charCodeAt(0) - 'a'.charCodeAt(0)]++);
-    const countsDesc = counts.sort((a, b) => b - a).filter((c) => c !== 0);
-    if (k >= countsDesc.length) return 0;
-    const takes = countsDesc.slice(k);
+    const counts: Map<string, number> = new Map();
+    s.split("").map((c) => counts.set(c, (counts.get(c) ?? 0) + 1));
+    if (k >= counts.size) return 0;
+    const sortedCounts = Array.from(counts.values()).sort((a, b) => a - b);
+    const takes = sortedCounts.slice(0, counts.size - k);
     return takes.reduce((a, b) => a + b, 0);
 };

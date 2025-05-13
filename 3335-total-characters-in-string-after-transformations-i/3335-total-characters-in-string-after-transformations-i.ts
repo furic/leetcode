0e3d6db81371ac09e3b61f69289fafd255eed17a
Richard Fu <44884321+furic@users.noreply.github.com>
2025-05-13 17:19:11 +1000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.ts
@@ -0,0 +1,11 @@
+const lengthAfterTransformations = (s: string, t: number): number => {
+    const MOD = 1e7;
+    let counts: number[] = new Array(26).fill(0);
+    for (const c of s) {
+        counts[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+    }
+    for (let round = 0; round < t; round++) {
+        counts = [counts[25], (counts[25] + counts[0]) % MOD, ...counts.slice(1, -1)];
+    }
+    return counts.reduce((a, b) => ((a + b) % MOD), 0);
+};

9cf1ce778008593d199ae2f4bb69cca2ea4d0a9f
Richard Fu <44884321+furic@users.noreply.github.com>
2025-05-15 21:02:58 +1000

    Time: 1 ms (65.91%), Space: 59.4 MB (36.36%) - LeetHub

--- 2900-longest-unequal-adjacent-groups-subsequence-i/2900-longest-unequal-adjacent-groups-subsequence-i.ts
+++ 2900-longest-unequal-adjacent-groups-subsequence-i/2900-longest-unequal-adjacent-groups-subsequence-i.ts
@@ -1,7 +1,7 @@
 const getLongestSubsequence = (words: string[], groups: number[]): string[] => {
-    let result: string[] = [];
-    let lastGroup = -1;
-    for (let i = 0; i < words.length; i++) {
+    const result: string[] = [words[0]];
+    let lastGroup = groups[0];
+    for (let i = 1; i < words.length; i++) {
         if (lastGroup === groups[i]) continue;
         result.push(words[i]);
         lastGroup = groups[i];

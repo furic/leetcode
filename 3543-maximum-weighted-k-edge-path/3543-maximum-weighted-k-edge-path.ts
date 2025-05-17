e79a1d6aa10cde474acecb67d30a94538c8f884b
Richard Fu <44884321+furic@users.noreply.github.com>
2025-05-17 17:27:10 +1000

    Time: 361 ms (100%), Space: 91.3 MB (100%) - LeetHub

--- 3543-maximum-weighted-k-edge-path/3543-maximum-weighted-k-edge-path.ts
+++ 3543-maximum-weighted-k-edge-path/3543-maximum-weighted-k-edge-path.ts
@@ -7,15 +7,12 @@ const maxWeight = (n: number, edges: number[][], k: number, t: number): number =
     const memo = new Set();
     let maxSum = -1;
 
-    const dfs = (node: number, pathLength: number, sum: number) => {
-        // console.log(node, pathLength, sum)
-        if (pathLength === k) {
-            if (sum < t) {
-                maxSum = Math.max(maxSum, sum);
-            }
+    const dfs = (node: number, remaining: number, sum: number) => {
+        if (remaining === 0) {
+            if (sum < t) maxSum = Math.max(maxSum, sum);
             return;
         }
-        let key = `${node}-${pathLength}-${sum}`
+        let key = `${node}-${remaining}-${sum}`
 
         if (memo.has(key)) return;
         memo.add(key);
@@ -23,12 +20,12 @@ const maxWeight = (n: number, edges: number[][], k: number, t: number): number =
         for (const [neighbor, weight] of graph[node]) {
             const newSum = sum + weight;
             if (newSum >= t) continue;
-            dfs(neighbor, pathLength + 1, newSum);
+            dfs(neighbor, remaining - 1, newSum);
         }
     };
 
     for (let i = 0; i < n; i++) {
-        dfs(i, 0, 0);
+        dfs(i, k, 0);
     }
 
     return maxSum;

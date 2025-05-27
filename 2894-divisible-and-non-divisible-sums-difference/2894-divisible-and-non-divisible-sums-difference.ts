d0e31dafceed8c3ebdec11fe16a65e23855fbbb1
Richard Fu <44884321+furic@users.noreply.github.com>
2025-05-27 19:52:37 +1000

    Time: 0 ms (100%), Space: 57.8 MB (37.29%) - LeetHub

--- 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.ts
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.ts
@@ -1,3 +1,9 @@
-const differenceOfSums = (n: number, m: number): number =>
-    Array.from({ length: n }, (_, i) => i + 1)
-        .reduce((acc, num) => acc + (num % m ? num : -num), 0);
+const differenceOfSums = (n: number, m: number): number => {
+    // Array.from({ length: n }, (_, i) => i + 1)
+    //     .reduce((acc, num) => acc + (num % m ? num : -num), 0);
+    let result = 0
+    for (let i = 1; i <= n; i++) {
+        result += i % m ? i : -i;
+    }
+    return result;
+}

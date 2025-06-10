4247d8e5079601136b982acc9fa797178d2cc46c
Richard Fu <44884321+furic@users.noreply.github.com>
2025-06-10 20:50:14 +1000

    Time: 2 ms (90.91%), Space: 57.7 MB (95.45%) - LeetHub

---
+++ 3442-maximum-difference-between-even-and-odd-frequency-i/3442-maximum-difference-between-even-and-odd-frequency-i.ts
@@ -0,0 +1,16 @@
+const maxDifference = (s: string): number => {
+    const map = s.split('').reduce(
+        (acc, char) => acc.set(char, (acc.get(char) || 0) + 1),
+        new Map<string, number>()
+    );
+
+    const [maxOdd, minEven] = Array.from(map.values()).reduce(
+        (acc, freq) => [
+            Math.max(freq % 2 ? freq : 0, acc[0]),
+            Math.min(freq % 2 === 0 ? freq : Infinity, acc[1]),
+        ],
+        [0, Infinity]
+    );
+
+    return maxOdd - minEven;
+};

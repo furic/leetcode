d6d7e0ff222badbb3c8f0e98f56168f69c055baa
Richard Fu <44884321+furic@users.noreply.github.com>
2025-06-17 21:15:07 +1000

    Time: 18 ms (80%), Space: 68.9 MB (0%) - LeetHub

--- 3405-count-the-number-of-arrays-with-k-matching-adjacent-elements/3405-count-the-number-of-arrays-with-k-matching-adjacent-elements.ts
+++ 3405-count-the-number-of-arrays-with-k-matching-adjacent-elements/3405-count-the-number-of-arrays-with-k-matching-adjacent-elements.ts
@@ -22,8 +22,6 @@ const comb = (n: number, m: number): bigint => {
 };
 
 const countGoodArrays = (n: number, m: number, k: number): number => {
-
-
     if (fact[0] === 0n) {
         fact[0] = 1n;
         for (let i = 1; i < MAX; i++) {
@@ -40,54 +38,3 @@ const countGoodArrays = (n: number, m: number, k: number): number => {
     res = (res * qpow(m - 1, n - k - 1)) % MOD;
     return Number(res);
 };
-
-// const MOD: bigint = BigInt(1e9 + 7);
-// const MX: number = 100000;
-
-// const fact: bigint[] = new Array(MX).fill(0n);
-// const invFact: bigint[] = new Array(MX).fill(0n);
-// let built: boolean = false;
-
-// function qpow(x: number | bigint, n: number | bigint): bigint {
-//     x = BigInt(x);
-//     n = BigInt(n);
-//     let res = 1n;
-//     while (n > 0n) {
-//         if (n & 1n) {
-//             res = (res * x) % MOD;
-//         }
-//         x = (x * x) % MOD;
-//         n >>= 1n;
-//     }
-//     return res;
-// }
-
-// function init(): void {
-//     if (fact[0] != 0n) {
-//         return;
-//     }
-//     fact[0] = 1n;
-//     for (let i = 1; i < MX; i++) {
-//         fact[i] = (fact[i - 1] * BigInt(i)) % MOD;
-//     }
-
-//     invFact[MX - 1] = qpow(fact[MX - 1], MOD - 2n);
-//     for (let i = MX - 2; i >= 0; i--) {
-//         invFact[i] = (invFact[i + 1] * BigInt(i + 1)) % MOD;
-//     }
-// }
-
-// function comb(n: number, m: number): bigint {
-//     if (m < 0 || m > n) {
-//         return 0n;
-//     }
-//     return (((fact[n] * invFact[m]) % MOD) * invFact[n - m]) % MOD;
-// }
-
-// function countGoodArrays(n: number, m: number, k: number): number {
-//     init();
-//     let res = comb(n - 1, k);
-//     res = (res * BigInt(m)) % MOD;
-//     res = (res * qpow(m - 1, n - k - 1)) % MOD;
-//     return Number(res);
-// }

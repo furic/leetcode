97df85ea4ea10518caf1723c239200e51626d00d
Richard Fu <44884321+furic@users.noreply.github.com>
2025-06-18 20:30:36 +1000

    Time: 243 ms (0%), Space: 86.1 MB (60%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.ts
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.ts
@@ -1,11 +1,46 @@
 const divideArray = (nums: number[], k: number): number[][] => {
-    nums.sort((a, b) => a - b);
-    const ans = [];
-    for (let i = 0; i < nums.length; i += 3) {
-        if (nums[i + 2] - nums[i] > k) {
-            return [];
-        }
-        ans.push([nums[i], nums[i + 1], nums[i + 2]]);
+    // nums.sort((a, b) => a - b);
+    // const ans = [];
+    // for (let i = 0; i < nums.length; i += 3) {
+    //     if (nums[i + 2] - nums[i] > k) {
+    //         return [];
+    //     }
+    //     ans.push([nums[i], nums[i + 1], nums[i + 2]]);
+    // }
+    // return ans;
+    const n = nums.length;
+    const freq: number[] = new Array(100001).fill(0);
+    let xMax = 0;
+
+    for (const x of nums) {
+        freq[x]++;
+        xMax = Math.max(x, xMax);
     }
+
+    const ans: number[][] = new Array(n / 3);
+    const vec3: number[] = new Array(3);
+    let count = 0;
+    let vec_m = 0;
+    let i = 0;
+
+    for (let x = 1; x <= xMax; x++) {
+        while (freq[x] > 0) {
+            freq[x]--;
+            if (count % 3 === 0) {
+                vec_m = x;
+            }
+            if (x > vec_m + k) {
+                return [];
+            }
+            vec3[count] = x;
+
+            if (count % 3 === 2) {
+                ans[i++] = [...vec3]; // copy to avoid mutation
+                count = -1;
+            }
+            count++;
+        }
+    }
+
     return ans;
 };

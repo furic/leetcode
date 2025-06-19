40351be197210fe687bf3116c258e223955f41f8
Richard Fu <44884321+furic@users.noreply.github.com>
2025-06-19 21:09:54 +1000

    Time: 110 ms (64.29%), Space: 73.3 MB (42.86%) - LeetHub

--- 2294-partition-array-such-that-maximum-difference-is-k/2294-partition-array-such-that-maximum-difference-is-k.ts
+++ 2294-partition-array-such-that-maximum-difference-is-k/2294-partition-array-such-that-maximum-difference-is-k.ts
@@ -1,35 +1,12 @@
 const partitionArray = (nums: number[], k: number): number => {
-    if (nums.length === 1) return 1;
-
-    const freq = new Array(100001).fill(0);
-    let maxVal = -1;
-
-    for (const x of nums) {
-        freq[x]++;
-        maxVal = Math.max(maxVal, x);
-    }
-
-    let partitions = 0;
-
-    for (let x = 0; x <= maxVal;) {
-        // Skip unused numbers
-        while (x <= maxVal && freq[x] === 0) {
-            x++;
+    nums.sort((a, b) => a - b);
+    let ans = 1;
+    let left = nums[0];
+    for (let num of nums) {
+        if (num - left > k) {
+            ans++;
+            left = num;
         }
-        if (x > maxVal) break;
-
-        // Start a new group from current x
-        partitions++;
-        const end = x + k;
-
-        // Mark all numbers from x to x+k as used
-        for (let y = x; y <= end; y++) {
-            freq[y] = 0;
-        }
-
-        // Move x to the next unprocessed number
-        x = end + 1;
     }
-
-    return partitions;
+    return ans;
 };

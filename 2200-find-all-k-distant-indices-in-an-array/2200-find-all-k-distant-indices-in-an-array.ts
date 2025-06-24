0def033966b492e1939265b965d4291f54244fe8
Richard Fu <44884321+furic@users.noreply.github.com>
2025-06-24 20:19:34 +1000

    Time: 0 ms (100%), Space: 58.7 MB (44.44%) - LeetHub

--- 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.ts
+++ 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.ts
@@ -1,2 +1,13 @@
-const findKDistantIndices = (nums: number[], key: number, k: number): number[] =>
-  nums.map((_, i) => i).filter(i => nums.some((val, j) => val === key && Math.abs(i - j) <= k));
+const findKDistantIndices = (nums: number[], key: number, k: number): number[] => {
+    const res: number[] = [];
+    let next = 0;
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] === key) {
+            const start = Math.max(next, j - k);
+            const end = Math.min(nums.length - 1, j + k);
+            for (let i = start; i <= end; i++) res.push(i);
+            next = end + 1;
+        }
+    }
+    return res;
+}

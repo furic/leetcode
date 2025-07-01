1957ac73a03b796d3120ffa298aa406d2e75ac20
Richard Fu <44884321+furic@users.noreply.github.com>
2025-07-01 17:47:57 +1000

    Time: 56 ms (16.07%), Space: 58.2 MB (8.93%) - LeetHub

---
+++ 3330-find-the-original-typed-string-i/3330-find-the-original-typed-string-i.ts
@@ -0,0 +1,10 @@
+const possibleStringCount = (word: string): number => {
+    let n = word.length,
+        ans = 1;
+    for (let i = 1; i < n; ++i) {
+        if (word[i - 1] === word[i]) {
+            ++ans;
+        }
+    }
+    return ans;
+};

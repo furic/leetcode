eece899ca0cc46ecc580eaf0a99c94ccdf6b2b9e
Richard Fu <44884321+furic@users.noreply.github.com>
2025-07-07 18:05:01 +1000

    Time: 6 ms (100%), Space: 63.5 MB (100%) - LeetHub

--- 3606-coupon-code-validator/3606-coupon-code-validator.ts
+++ 3606-coupon-code-validator/3606-coupon-code-validator.ts
@@ -1,6 +1,6 @@
 const validateCoupons = (code: string[], businessLine: string[], isActive: boolean[]): string[] =>
-  code
-    .map((c, i) => ({ c, b: businessLine[i], a: isActive[i] }))
-    .filter(({ c, b, a }) => a && c && /^[a-zA-Z0-9_]+$/.test(c) && ["electronics", "grocery", "pharmacy", "restaurant"].includes(b))
-    .sort((x, y) => ["electronics", "grocery", "pharmacy", "restaurant"].indexOf(x.b) - ["electronics", "grocery", "pharmacy", "restaurant"].indexOf(y.b) || (x.c < y.c ? -1 : x.c > y.c ? 1 : 0))
-    .map(({ c }) => c);
+    code
+        .map((c, i) => ({ c, b: businessLine[i], a: isActive[i] }))
+        .filter(({ c, b, a }) => a && c && /^[a-zA-Z0-9_]+$/.test(c) && ["electronics", "grocery", "pharmacy", "restaurant"].includes(b))
+        .sort((x, y) => ["electronics", "grocery", "pharmacy", "restaurant"].indexOf(x.b) - ["electronics", "grocery", "pharmacy", "restaurant"].indexOf(y.b) || (x.c < y.c ? -1 : x.c > y.c ? 1 : 0))
+        .map(({ c }) => c);

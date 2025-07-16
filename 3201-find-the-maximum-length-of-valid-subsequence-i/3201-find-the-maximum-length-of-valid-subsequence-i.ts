91b79518fdd5ebbdf070502d8ece3cd408803dd2
Richard Fu <44884321+furic@users.noreply.github.com>
2025-07-16 22:06:11 +1000

    Time: 27 ms (25%), Space: 76.6 MB (25%) - LeetHub

--- 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.ts
+++ 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.ts
@@ -1,7 +1,7 @@
 const maximumLength = (nums: number[]): number => {
     const evens = nums.map((n) => n % 2 === 0);
-    const oddCount = evens.filter((even) => even).length;
-    const evenCount = evens.filter((even) => !even).length;
+    const evenCount = evens.filter((even) => even).length;
+    const oddCount = evens.length - evenCount;
     let alterCount = 1;
     let lastEven = evens[0];
     for (let i = 1; i < evens.length; i++) {

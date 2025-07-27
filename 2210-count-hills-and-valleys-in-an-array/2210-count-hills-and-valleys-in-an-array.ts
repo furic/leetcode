794f054959c99333793724e9287b7252572242d4
Richard Fu <44884321+furic@users.noreply.github.com>
2025-07-27 20:24:04 +1000

    Time: 0 ms (100%), Space: 56.2 MB (26.32%) - LeetHub

---
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.ts
@@ -0,0 +1,15 @@
+function countHillValley(nums: number[]): number {
+    let count = 0;
+    let left = 0;
+
+    for (let i = 1; i < nums.length - 1; i++) {
+        if (nums[i] !== nums[i + 1]) {
+            if (nums[i] > nums[left] && nums[i] > nums[i + 1] || nums[i] < nums[left] && nums[i] < nums[i + 1]) {
+                count++;
+            }
+            left = i;
+        }
+    }
+    return count;
+
+};

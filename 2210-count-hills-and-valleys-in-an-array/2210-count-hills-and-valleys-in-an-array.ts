1141dcd5bac8cc375be987f917f48e3f38a95536
Richard Fu <44884321+furic@users.noreply.github.com>
2025-07-27 20:29:09 +1000

    Time: 0 ms (100%), Space: 55.8 MB (47.37%) - LeetHub

--- 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.ts
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.ts
@@ -1,15 +1,21 @@
-function countHillValley(nums: number[]): number {
-    let count = 0;
-    let left = 0;
+const countHillValley = (nums: number[]): number => {
+    let hillValleyCount = 0;
+    let lastUniqueIndex = 0;
 
-    for (let i = 1; i < nums.length - 1; i++) {
-        if (nums[i] !== nums[i + 1]) {
-            if (nums[i] > nums[left] && nums[i] > nums[i + 1] || nums[i] < nums[left] && nums[i] < nums[i + 1]) {
-                count++;
-            }
-            left = i;
-        }
+    for (let current = 1; current < nums.length - 1; current++) {
+        if (nums[current] === nums[current + 1]) continue;
+
+        const left = nums[lastUniqueIndex];
+        const center = nums[current];
+        const right = nums[current + 1];
+
+        const isHill = center > left && center > right;
+        const isValley = center < left && center < right;
+
+        if (isHill || isValley) hillValleyCount++;
+
+        lastUniqueIndex = current;
     }
-    return count;
 
+    return hillValleyCount;
 };

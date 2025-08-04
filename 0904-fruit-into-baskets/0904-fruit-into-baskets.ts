49ecc3bdc23d4c29ff72df4945875d8e2e453737
Richard Fu <44884321+furic@users.noreply.github.com>
2025-08-04 17:58:26 +1000

    Time: 3 ms (99.31%), Space: 63.1 MB (98.61%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.ts
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.ts
@@ -1,29 +1,61 @@
+// function totalFruit(fruits: number[]): number {
+//     let start = 0
+//     let start2 = -1
+//     let currType = -1
+//     let type2 = -1
+//     let max = 0
+//     for (let i = 0; i < fruits.length; i++) {
+//         if (fruits[i] !== currType) {
+//             if (currType === -1) {
+//                 start = i
+//                 currType = fruits[i]
+//             } else if (fruits[i] === type2 || type2 === -1) {
+//                 type2 = currType
+//                 currType = fruits[i]
+//                 start2 = i
+//             } else if (fruits[i] !== type2) {
+//                 max = Math.max(max, i - start)
+//                 start = start2
+//                 start2 = i
+//                 type2 = currType
+//                 currType = fruits[i]
+//             } else {
+//                 console.log(`[DBG] can there be else?`)
+//             }
+//         }
+//     }
+//     max = Math.max(max, fruits.length - start)
+//     return max
+// }
+
 function totalFruit(fruits: number[]): number {
-    let start = 0
-    let start2 = -1
-    let currType = -1
-    let type2 = -1
-    let max = 0
+    let maxFruits = 0;
+    let windowStart = 0;
+
+    let lastFruitType = -1;
+    let secondFruitType = -1;
+    let lastFruitStartIndex = -1;
+
     for (let i = 0; i < fruits.length; i++) {
-        if (fruits[i] !== currType) {
-            if (currType === -1) {
-                start = i
-                currType = fruits[i]
-            } else if (fruits[i] === type2 || type2 === -1) {
-                type2 = currType
-                currType = fruits[i]
-                start2 = i
-            } else if (fruits[i] !== type2) {
-                max = Math.max(max, i - start)
-                start = start2
-                start2 = i
-                type2 = currType
-                currType = fruits[i]
+        const currentFruit = fruits[i];
+
+        if (currentFruit !== lastFruitType) {
+            if (secondFruitType === -1 || currentFruit === secondFruitType) {
+                // Swap roles
+                secondFruitType = lastFruitType;
+                lastFruitType = currentFruit;
+                lastFruitStartIndex = i;
             } else {
-                console.log(`[DBG] can there be else?`)
+                // New third fruit type encountered
+                maxFruits = Math.max(maxFruits, i - windowStart);
+                windowStart = lastFruitStartIndex;
+                secondFruitType = lastFruitType;
+                lastFruitType = currentFruit;
+                lastFruitStartIndex = i;
             }
         }
     }
-    max = Math.max(max, fruits.length - start)
-    return max
+
+    maxFruits = Math.max(maxFruits, fruits.length - windowStart);
+    return maxFruits;
 }

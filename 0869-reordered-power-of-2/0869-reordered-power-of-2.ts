afb9dbe3371c2edfad51e8a514ac29bf8e26a042
Richard Fu <44884321+furic@users.noreply.github.com>
2025-08-10 19:11:31 +1000

    Time: 1 ms (60%), Space: 57 MB (80%) - LeetHub

--- 0869-reordered-power-of-2/0869-reordered-power-of-2.ts
+++ 0869-reordered-power-of-2/0869-reordered-power-of-2.ts
@@ -1,9 +1,9 @@
 const reorderedPowerOf2 = (n: number): boolean => {
-    const countDigits = x => [...String(x)].sort().join('');
+    const countDigits = x => x.toString().split('').sort().join('');
     const target = countDigits(n);
-    
-    for (let i = 0; i < 31; i++)
+    const limit = 1e9;
+    for (let i = 0; i < 31; i++) {
         if (countDigits(1 << i) === target) return true;
-
+    }
     return false;
 };

dd466d938a8c519c4adec1ee494b4f35a9cd21d6
Richard Fu <44884321+furic@users.noreply.github.com>
2025-08-10 14:34:17 +1000

    Time: 2 ms (25%), Space: 71 MB (62.5%) - LeetHub

--- 3644-maximum-k-to-sort-a-permutation/3644-maximum-k-to-sort-a-permutation.ts
+++ 3644-maximum-k-to-sort-a-permutation/3644-maximum-k-to-sort-a-permutation.ts
@@ -3,14 +3,7 @@ const sortPermutation = (nums: number[]): number => {
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] === i) continue;
         const k = nums[i] & i;
-        // if (res === -1) {
-        //     res = k;
-        // } else if (k < res) {
-        //     return 0;
-        // }
-        // const iIndex = nums.indexOf(i);
         res = res & k;
-        // [nums[i], nums[iIndex]] = [nums[iIndex], nums[i]];
     }
     return res === Number.MAX_SAFE_INTEGER ? 0 : res;
 };

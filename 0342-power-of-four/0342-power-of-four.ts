be12c17043d7e2bc96f59fd1b662ed4320fe8ce8
Richard Fu <44884321+furic@users.noreply.github.com>
2025-08-15 18:43:03 +1000

    Time: 0 ms (100%), Space: 58.2 MB (18.75%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.ts
+++ 0342-power-of-four/0342-power-of-four.ts
@@ -1,3 +1,1 @@
-function isPowerOfFour(n: number): boolean {
-    return n > 0 && (n & (n - 1)) === 0 && (n & 0x55555555) !== 0;
-};
+const isPowerOfFour = (n: number): boolean => n > 0 && (n & (n - 1)) === 0 && (n & 0x55555555) !== 0;

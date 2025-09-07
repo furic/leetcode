e9486f85043f17f479f6d803742a33f3f8f2e571
Richard Fu <44884321+furic@users.noreply.github.com>
2025-09-07 14:15:44 +1000

    Time: 133 ms (100%), Space: 80.6 MB (100%) - LeetHub

--- 3675-minimum-operations-to-transform-string/3675-minimum-operations-to-transform-string.ts
+++ 3675-minimum-operations-to-transform-string/3675-minimum-operations-to-transform-string.ts
@@ -1,11 +1,2 @@
-const minOperations = (s: string): number => {
-    let maxSteps = 0;
-
-    for (const char of s) {
-        // Compute how many steps it takes to turn `char` into 'a' using circular alphabet
-        const stepsToA = (26 - (char.charCodeAt(0) - 'a'.charCodeAt(0))) % 26;
-        maxSteps = Math.max(maxSteps, stepsToA);
-    }
-
-    return maxSteps;
-};
+const minOperations = (s: string): number => 
+    s.split('').reduce((max, char) => Math.max(max, (26 - (char.charCodeAt(0) - 'a'.charCodeAt(0))) % 26), 0);

5a0c3927b9dbacc70d6bcc155e9ee4d81a460592
Richard Fu <44884321+furic@users.noreply.github.com>
2025-09-08 18:54:44 +1000

    Time: 1 ms (21.43%), Space: 55.9 MB (28.57%) - LeetHub

--- 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.ts
+++ 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.ts
@@ -1,9 +1,9 @@
-function getNoZeroIntegers(n: number): number[] {
-    for (let A = 1; A < n; A++) {
-        const B = n - A;
-        if (!A.toString().includes("0") && !B.toString().includes("0")) {
-            return [A, B];
+const getNoZeroIntegers = (n: number): number[] => {
+    for (let firstNumber = 1; firstNumber < n; firstNumber++) {
+        const secondNumber = n - firstNumber;
+        if (!firstNumber.toString().includes("0") && !secondNumber.toString().includes("0")) {
+            return [firstNumber, secondNumber];
         }
     }
     return [];
-}
+};

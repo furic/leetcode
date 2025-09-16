08829cce489084bbbae8feec1e1fb82b77082fc6
Richard Fu <44884321+furic@users.noreply.github.com>
2025-09-16 19:05:52 +1000

    Time: 23 ms (100%), Space: 81.6 MB (50%) - LeetHub

---
+++ 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.ts
@@ -0,0 +1,28 @@
+function replaceNonCoprimes(nums: number[]): number[] {
+    const stack = [];
+    
+    const gcd = (a, b) => {
+        while (b !== 0) {
+            const t = b;
+            b = a % b;
+            a = t;
+        }
+        return a;
+    };
+    
+    for (let num of nums) {
+        while (stack.length > 0) {
+            const top = stack[stack.length - 1];
+            const g = gcd(top, num);
+            if (g === 1) {
+                break;
+            }
+            stack.pop();
+            num = (top / g) * num;
+        }
+        stack.push(num);
+    }
+    
+    return stack;
+
+};

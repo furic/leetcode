2dfa83421ae1d00cd9c0ebcce5f7bf9a2fdf6f42
Richard Fu <44884321+furic@users.noreply.github.com>
2025-09-21 22:30:04 +1000

    Time: 467 ms (100%), Space: 131.1 MB (100%) - LeetHub

--- 1912-design-movie-rental-system/1912-design-movie-rental-system.ts
+++ 1912-design-movie-rental-system/1912-design-movie-rental-system.ts
@@ -1,62 +1,85 @@
 class MovieRentingSystem {
-    private price: Map<string, number>;                           
-    private movieShops: Map<number, Array<[number, number]>>;     
-    private rented: Set<string>;                                  
-    private encode(shop: number, movie: number): string {
-        return `${shop}#${movie}`;
-    }
-    constructor(n: number, entries: number[][]) {
-        this.price = new Map();
-        this.movieShops = new Map();
-        this.rented = new Set();
-
-        for (const [shop, movie, p] of entries) {
-            const key = this.encode(shop, movie);
-            this.price.set(key, p);
-            if (!this.movieShops.has(movie)) this.movieShops.set(movie, []);
-            this.movieShops.get(movie)!.push([p, shop]);
-        }
-
-        for (const arr of this.movieShops.values()) {
-            arr.sort((a, b) => (a[0] - b[0]) || (a[1] - b[1]));
-        }
+    // Maps "shop#movie" -> rental price
+    private priceByShopMovie: Map<string, number>;
+    
+    // Maps movie ID -> sorted list of [price, shopId] for all shops with that movie
+    private shopsByMovie: Map<number, Array<[number, number]>>;
+    
+    // Set of currently rented "shop#movie" keys
+    private currentlyRented: Set<string>;
+    
+    // Create unique key for shop-movie combination
+    private createKey(shopId: number, movieId: number): string {
+        return `${shopId}#${movieId}`;
     }
 
-    search(movie: number): number[] {
-        const res: number[] = [];
-        const arr = this.movieShops.get(movie) || [];
-        for (const [p, shop] of arr) {
-            if (!this.rented.has(this.encode(shop, movie))) {
-                res.push(shop);
-                if (res.length === 5) break;
+    constructor(totalShops: number, entries: number[][]) {
+        this.priceByShopMovie = new Map();
+        this.shopsByMovie = new Map();
+        this.currentlyRented = new Set();
+
+        // Build data structures from initial inventory
+        for (const [shopId, movieId, rentalPrice] of entries) {
+            const shopMovieKey = this.createKey(shopId, movieId);
+            this.priceByShopMovie.set(shopMovieKey, rentalPrice);
+            
+            // Group shops by movie ID
+            if (!this.shopsByMovie.has(movieId)) {
+                this.shopsByMovie.set(movieId, []);
+            }
+            this.shopsByMovie.get(movieId)!.push([rentalPrice, shopId]);
+        }
+
+        // Pre-sort shops for each movie by price, then by shop ID
+        for (const shopList of this.shopsByMovie.values()) {
+            shopList.sort((a, b) => (a[0] - b[0]) || (a[1] - b[1]));
+        }
+    }
+
+    search(movieId: number): number[] {
+        const availableShops: number[] = [];
+        const shopsWithMovie = this.shopsByMovie.get(movieId) || [];
+        
+        // Find up to 5 cheapest unrented copies
+        for (const [price, shopId] of shopsWithMovie) {
+            if (!this.currentlyRented.has(this.createKey(shopId, movieId))) {
+                availableShops.push(shopId);
+                if (availableShops.length === 5) break;
             }
         }
-        return res;
+        
+        return availableShops;
     }
 
-    rent(shop: number, movie: number): void {
-        this.rented.add(this.encode(shop, movie));
+    rent(shopId: number, movieId: number): void {
+        this.currentlyRented.add(this.createKey(shopId, movieId));
     }
 
-    drop(shop: number, movie: number): void {
-        this.rented.delete(this.encode(shop, movie));
+    drop(shopId: number, movieId: number): void {
+        this.currentlyRented.delete(this.createKey(shopId, movieId));
     }
 
     report(): number[][] {
-        const tmp: Array<[number, number, number]> = []; // [price, shop, movie]
-        for (const key of this.rented) {
-            const [shopStr, movieStr] = key.split('#');
-            const shop = Number(shopStr);
-            const movie = Number(movieStr);
-            const p = this.price.get(key)!;
-            tmp.push([p, shop, movie]);
+        // Collect all rented movies with their details
+        const rentedMovies: Array<[number, number, number]> = []; // [price, shopId, movieId]
+        
+        for (const shopMovieKey of this.currentlyRented) {
+            const [shopIdStr, movieIdStr] = shopMovieKey.split('#');
+            const shopId = Number(shopIdStr);
+            const movieId = Number(movieIdStr);
+            const price = this.priceByShopMovie.get(shopMovieKey)!;
+            rentedMovies.push([price, shopId, movieId]);
         }
-        tmp.sort((a, b) => (a[0] - b[0]) || (a[1] - b[1]) || (a[2] - b[2]));
+        
+        // Sort by price, then shop ID, then movie ID
+        rentedMovies.sort((a, b) => (a[0] - b[0]) || (a[1] - b[1]) || (a[2] - b[2]));
 
-        const res: number[][] = [];
-        for (let i = 0; i < tmp.length && i < 5; i++) {
-            res.push([tmp[i][1], tmp[i][2]]);
+        // Return top 5 as [shopId, movieId] pairs
+        const topRentals: number[][] = [];
+        for (let index = 0; index < rentedMovies.length && index < 5; index++) {
+            topRentals.push([rentedMovies[index][1], rentedMovies[index][2]]);
         }
-        return res;
+        
+        return topRentals;
     }
 }

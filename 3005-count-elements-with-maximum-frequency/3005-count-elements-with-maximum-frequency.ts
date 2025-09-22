78482fe438a2fd4b010bd3a70b9d4eafca56c8b0
Richard Fu <44884321+furic@users.noreply.github.com>
2025-09-22 18:54:14 +1000

    Time: 5 ms (21.57%), Space: 58.4 MB (29.41%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.ts
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.ts
@@ -1,9 +1,20 @@
-function maxFrequencyElements(nums: number[]): number {
-    const freq = nums.reduce((acc, n) => {
-    acc[n] = (acc[n] || 0) + 1;
-    return acc;
-    }, {} as Record<number, number>);
-    const maxFreq = Math.max(...Object.values(freq));
-    const total = Object.values(freq).reduce((sum, count) => sum + (count === maxFreq ? count : 0), 0);
-    return total;
+const maxFrequencyElements = (nums: number[]): number => {
+    if (nums.length === 0) return 0;
+
+    const elementFrequencies = new Map<number, number>();
+    
+    for (const element of nums) {
+        elementFrequencies.set(element, (elementFrequencies.get(element) || 0) + 1);
+    }
+
+    const maxFrequency = Math.max(...elementFrequencies.values());
+    
+    let totalElementsWithMaxFrequency = 0;
+    for (const [_, frequency] of elementFrequencies) {
+        if (frequency === maxFrequency) {
+            totalElementsWithMaxFrequency += frequency;
+        }
+    }
+
+    return totalElementsWithMaxFrequency;
 };

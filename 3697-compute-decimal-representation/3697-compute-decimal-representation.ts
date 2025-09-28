5de197b526c4d890b118ca6f39b12759902cb756
Richard Fu <44884321+furic@users.noreply.github.com>
2025-09-28 14:08:05 +1000

    Time: 1 ms (92%), Space: 58.4 MB (96%) - LeetHub

--- 3697-compute-decimal-representation/3697-compute-decimal-representation.ts
+++ 3697-compute-decimal-representation/3697-compute-decimal-representation.ts
@@ -1,19 +1,9 @@
-const decimalRepresentation = (n: number): number[] => {
-    const base10Components: number[] = [];
-    let positionalValue = 1; // Represents 10^0, 10^1, 10^2, etc.
-    
-    while (n > 0) {
-        const digit = n % 10;
-        
-        // Only add non-zero digits as base-10 components
-        if (digit !== 0) {
-            const component = digit * positionalValue;
-            base10Components.unshift(component);
-        }
-        
-        n = Math.floor(n / 10);
-        positionalValue *= 10;
-    }
-    
-    return base10Components;
-};
+function decimalRepresentation(n: number): number[] {
+  return (n + "")
+    .split("")
+    .reverse()
+    .map((d, i) => d + "0".repeat(i))
+    .reverse()
+    .map(d => parseInt(d, 10))
+    .filter(d => d !== 0)
+}

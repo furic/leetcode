02ecea8be7df1fec942cabd0a7c79313f2bd1b26
Richard Fu <44884321+furic@users.noreply.github.com>
2025-10-04 22:41:30 +1000

    Time: 1 ms (90.6%), Space: 54.9 MB (99.3%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.ts
+++ 0020-valid-parentheses/0020-valid-parentheses.ts
@@ -1,22 +1,20 @@
 function isValid(s: string): boolean {
-    let map = new Map([
-        ["}", "{"],
-        [")", "("],
-        ["]", "["],
-    ])
-
-    let stack =  []
-
-    for (let i of s) {
-        if (map.has(i)) {
-            let pop = stack.pop()
-            if (pop !== map.get(i)) {
-                return false
-            }
+    const stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(' || s[i] === '[' || s[i] === '{') {
+            stack.push(s[i])
         } else {
-            stack.push(i)
+            if (stack.length === 0) {
+                return false;
+            }
+            const top = stack.pop();
+            if (s[i] == ')' && top != '(') return false;
+
+            if (s[i] == ']' && top != '[') return false;
+
+            if (s[i] == '}' && top != '{') return false;
         }
     }
 
-    return !stack.length
+    return stack.length === 0;
 };

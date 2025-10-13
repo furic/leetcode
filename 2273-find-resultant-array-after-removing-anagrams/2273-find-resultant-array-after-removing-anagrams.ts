e43b3a4017d0b205acc7598ef0eba2f784b04d89
Richard Fu <44884321+furic@users.noreply.github.com>
2025-10-13 20:29:03 +1100

    Time: 6 ms (86.21%), Space: 62.3 MB (27.59%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.ts
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.ts
@@ -1,23 +1,17 @@
-function removeAnagrams(words: string[]): string[] {
-    let res: string[] = [words[0]]; // result array
-    let n: number = words.length;
-
-    for (let i = 1; i < n; i++) {
-        if (!compare(words[i], words[i - 1])) {
-            res.push(words[i]);
+const removeAnagrams = (words: string[]): string[] => {
+    const result: string[] = [];
+    let previousSortedWord = "";
+    
+    for (const word of words) {
+        // Create canonical form by sorting characters
+        const sortedWord = word.split("").sort().join("");
+        
+        // Only keep word if it's not an anagram of the previous word
+        if (sortedWord !== previousSortedWord) {
+            result.push(word);
+            previousSortedWord = sortedWord;
         }
     }
-    return res;
-}
-
-// determine if two words are anagrams
-function compare(word1: string, word2: string): boolean {
-    let freq: number[] = new Array(26).fill(0);
-    for (let ch of word1) {
-        freq[ch.charCodeAt(0) - "a".charCodeAt(0)]++;
-    }
-    for (let ch of word2) {
-        freq[ch.charCodeAt(0) - "a".charCodeAt(0)]--;
-    }
-    return freq.every((x) => x === 0);
-}
+    
+    return result;
+};

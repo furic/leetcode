c973b88b351046e8b4039b108bc9adc2a356bb54
Richard Fu <44884321+furic@users.noreply.github.com>
2025-10-27 10:30:00 +1100

    Time: 151 ms (100%), Space: 77.6 MB (100%) - LeetHub

--- 3727-maximum-alternating-sum-of-squares/3727-maximum-alternating-sum-of-squares.ts
+++ 3727-maximum-alternating-sum-of-squares/3727-maximum-alternating-sum-of-squares.ts
@@ -1,18 +1,15 @@
-function maxAlternatingSum(nums: number[]): number {
-    let ans = 0n;
-    nums = nums.map(Math.abs);
-    nums.sort((a, b) => a - b);
-
-    for (let i = 0; i < Math.floor(nums.length / 2); i++) {
-        const high = BigInt(nums[nums.length - i - 1]);
-        const low = BigInt(nums[i]);
-        ans += high * high - low * low;
+const maxAlternatingSum = (nums: number[]): number => {
+    nums = nums.sort((a, b) => Math.abs(b) - Math.abs(a));
+    let ans = 0, i = 0, left = 0, right = nums.length - 1;
+    while (left <= right) {
+        if (i % 2 === 0) {
+            ans += nums[left] * nums[left];
+            left++;
+        } else {
+            ans -= nums[right] * nums[right];
+            right--;
+        }
+        i++;
     }
-
-    if (nums.length % 2 === 1) {
-        const mid = BigInt(nums[Math.floor(nums.length / 2)]);
-        ans += mid * mid;
-    }
-
-    return Number(ans);
-}
+    return ans;
+};

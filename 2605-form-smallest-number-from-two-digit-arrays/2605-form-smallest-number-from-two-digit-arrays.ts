d8900fb66be6f2a01ca0f6ac50949f4668ce02ee
Richard Fu <44884321+furic@users.noreply.github.com>
2025-11-06 21:16:08 +1100

    Time: 1 ms (50%), Space: 56.6 MB (44.44%) - LeetHub

---
+++ 2605-form-smallest-number-from-two-digit-arrays/2605-form-smallest-number-from-two-digit-arrays.ts
@@ -0,0 +1,13 @@
+function minNumber(nums1: number[], nums2: number[]): number {
+    nums1.sort((a, b) => a - b);
+
+    for (let i = 0; i < nums1.length; i++) {
+        if (nums2.includes(nums1[i])) {
+            return nums1[i];
+        }
+    }
+
+    const firstChar = nums1[0];
+    const secondChar = Math.min(...nums2);
+    return Math.min(+`${firstChar}${secondChar}`, +`${secondChar}${firstChar}`)
+};

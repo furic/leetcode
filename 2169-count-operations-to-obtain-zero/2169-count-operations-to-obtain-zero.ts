592ca624272fd61955510abf2422a1d0c4fd106d
Richard Fu <44884321+furic@users.noreply.github.com>
2025-11-09 16:35:06 +1100

    Time: 1 ms (78.43%), Space: 54 MB (94.12%) - LeetHub

---
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.ts
@@ -0,0 +1,9 @@
+function countOperations(num1: number, num2: number): number {
+    let res = 0;
+    while (num1 && num2) {
+        res += Math.floor(num1 / num2);
+        num1 %= num2;
+        [num1, num2] = [num2, num1];
+    }
+    return res;
+}

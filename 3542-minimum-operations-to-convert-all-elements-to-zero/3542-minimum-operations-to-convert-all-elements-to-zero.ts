3e32282baaedd3292732bf95064b1e349f300816
Richard Fu <44884321+furic@users.noreply.github.com>
2025-11-10 18:34:17 +1100

    Time: 20 ms (16.67%), Space: 77.7 MB (33.33%) - LeetHub

--- 3542-minimum-operations-to-convert-all-elements-to-zero/3542-minimum-operations-to-convert-all-elements-to-zero.ts
+++ 3542-minimum-operations-to-convert-all-elements-to-zero/3542-minimum-operations-to-convert-all-elements-to-zero.ts
@@ -1,42 +1,15 @@
-const minOperations = (nums: number[]): number => {
-  const monotonicStack: number[] = [];
-  const frequencyMap = new Map<number, number>();
-  let currentMax = -1;
-  let operationCount = 0;
-
-  for (const num of nums) {
-    // If the stack is empty or non-decreasing, just push the current number
-    if (monotonicStack.length === 0 || monotonicStack[monotonicStack.length - 1] <= num) {
-      monotonicStack.push(num);
-    } else {
-      // Process and count all values in the stack that are greater than current number
-      while (monotonicStack.length && monotonicStack[monotonicStack.length - 1] > num) {
-        const popped = monotonicStack.pop()!;
-        frequencyMap.set(popped, (frequencyMap.get(popped) ?? 0) + 1);
-
-        if (popped !== currentMax) {
-          currentMax = popped;
-          operationCount++;
+function minOperations(nums: number[]): number {
+    const s = [];
+    let res = 0;
+    for (const a of nums) {
+        while (s.length && s[s.length - 1] > a) {
+            s.pop();
         }
-      }
-
-      monotonicStack.push(num);
-      currentMax = -1; // Reset for next segment
+        if (a === 0) continue;
+        if (!s.length || s[s.length - 1] < a) {
+            res++;
+            s.push(a);
+        }
     }
-  }
-
-  // Process remaining stack elements
-  while (monotonicStack.length) {
-    const popped = monotonicStack.pop()!;
-    if (popped === 0) continue;
-
-    frequencyMap.set(popped, (frequencyMap.get(popped) ?? 0) + 1);
-
-    if (popped !== currentMax) {
-      currentMax = popped;
-      operationCount++;
-    }
-  }
-
-  return operationCount;
-};
+    return res;
+}

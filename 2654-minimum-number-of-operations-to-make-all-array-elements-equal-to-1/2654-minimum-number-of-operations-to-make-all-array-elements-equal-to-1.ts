a501caf6cdbdb6c023a58b734b729721d1607a1f
Richard Fu <44884321+furic@users.noreply.github.com>
2025-11-12 19:09:25 +1100

    Time: 6 ms (0%), Space: 58.8 MB (0%) - LeetHub

--- 2654-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2654-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.ts
+++ 2654-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2654-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.ts
@@ -1,11 +1,13 @@
 function minOperations(nums: number[]): number {
-    const n = nums.length;
-    let num1 = 0;
-    let g = 0;
+    const n: number = nums.length;
+    let num1: number = 0;
+    let g: number = 0;
 
     const gcd = (a: number, b: number): number => {
         while (b !== 0) {
-            [a, b] = [b, a % b];
+            const temp: number = b;
+            b = a % b;
+            a = temp;
         }
         return a;
     };
@@ -20,14 +22,13 @@ function minOperations(nums: number[]): number {
     if (num1 > 0) {
         return n - num1;
     }
-
     if (g > 1) {
         return -1;
     }
 
-    let minLen = n;
+    let minLen: number = n;
     for (let i = 0; i < n; i++) {
-        let currentGcd = 0;
+        let currentGcd: number = 0;
         for (let j = i; j < n; j++) {
             currentGcd = gcd(currentGcd, nums[j]);
             if (currentGcd === 1) {
@@ -36,6 +37,5 @@ function minOperations(nums: number[]): number {
             }
         }
     }
-
     return minLen + n - 2;
 }

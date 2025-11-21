58bb93f1ea3fcc69c784bd99a6ed14af25b3d56d
Richard Fu <44884321+furic@users.noreply.github.com>
2025-11-21 19:09:11 +1100

    Time: 41 ms (100%), Space: 60 MB (75%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.ts
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.ts
@@ -1,8 +1,25 @@
-const countPalindromicSubsequence = (s: string): number => {
-    let result = 0;
-    for (const char of new Set(s)) {
-        const first = s.indexOf(char), last = s.lastIndexOf(char);
-        if (first < last) result += new Set(s.slice(first + 1, last)).size;
+function countPalindromicSubsequence(s: string): number {
+    const n = s.length;
+    const first = Array(26).fill(-1);
+    const last = Array(26).fill(-1);
+
+    for (let i = 0; i < n; i++) {
+        const c = s.charCodeAt(i) - 97;
+        if (first[c] === -1) first[c] = i;
+        last[c] = i;
     }
-    return result;
-};
+
+    let ans = 0;
+
+    for (let c = 0; c < 26; c++) {
+        if (first[c] !== -1 && last[c] - first[c] > 1) {
+            let mask = 0;
+            for (let i = first[c] + 1; i < last[c]; i++) {
+                mask |= 1 << (s.charCodeAt(i) - 97);
+            }
+            ans += mask.toString(2).split('1').length - 1;
+        }
+    }
+
+    return ans;
+}

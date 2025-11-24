3609d1d9329965bb8b6d382a0857eb6534d57e17
Richard Fu <44884321+furic@users.noreply.github.com>
2025-11-24 18:26:54 +1100

    Time: 2 ms (66.67%), Space: 59.2 MB (100%) - LeetHub

--- 1018-binary-prefix-divisible-by-5/1018-binary-prefix-divisible-by-5.ts
+++ 1018-binary-prefix-divisible-by-5/1018-binary-prefix-divisible-by-5.ts
@@ -4,7 +4,6 @@ const prefixesDivBy5 = (nums: number[]): boolean[] => {
 
     for (let i = 0; i < nums.length; i++) {
         // Build number bit by bit: shift left (×2) and add new bit, then mod 5
-        // (n << 1 | nums[i]) is equivalent to (n * 2 + nums[i])
         remainderMod5 = (remainderMod5 * 2 + nums[i]) % 5;
         result[i] = remainderMod5 === 0;
     }

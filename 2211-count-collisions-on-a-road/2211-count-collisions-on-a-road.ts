60ca100d6ac8953ef67d0a89645f8cd77cd97e0b
Richard Fu <44884321+furic@users.noreply.github.com>
2025-12-05 01:28:35 +1100

    Time: 12 ms (66.67%), Space: 60 MB (50%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.ts
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.ts
@@ -1,24 +1,20 @@
 function countCollisions(directions: string): number {
+    const n: number = directions.length;
+    let l: number = 0,
+        r: number = n - 1;
+
+    while (l < n && directions[l] === "L") {
+        l++;
+    }
+
+    while (r >= l && directions[r] === "R") {
+        r--;
+    }
+
     let res: number = 0;
-    let flag: number = -1;
-    for (let i = 0; i < directions.length; i++) {
-        const c: string = directions[i];
-        if (c === "L") {
-            if (flag >= 0) {
-                res += flag + 1;
-                flag = 0;
-            }
-        } else if (c === "S") {
-            if (flag > 0) {
-                res += flag;
-            }
-            flag = 0;
-        } else {
-            if (flag >= 0) {
-                flag++;
-            } else {
-                flag = 1;
-            }
+    for (let i = l; i <= r; i++) {
+        if (directions[i] !== "S") {
+            res++;
         }
     }
     return res;

42c14c2e3827d7c4aa017d7195d23c2da05908e4
Richard Fu <44884321+furic@users.noreply.github.com>
2025-12-08 18:47:35 +1100

    Time: 2435 ms (100%), Space: 62.9 MB (100%) - LeetHub

--- 3766-minimum-operations-to-make-binary-palindrome/3766-minimum-operations-to-make-binary-palindrome.ts
+++ 3766-minimum-operations-to-make-binary-palindrome/3766-minimum-operations-to-make-binary-palindrome.ts
@@ -1,25 +1,37 @@
-const minOperations = (nums: number[]): number[] => {
-    const isBinaryPalindrome = (n: number): boolean => {
-        const binary = n.toString(2);
-        return binary === binary.split('').reverse().join('');
-    };
-    
-    const findMinOps = (num: number): number => {
-        if (isBinaryPalindrome(num)) return 0;
-        
-        let dist = 1;
-        while (true) {
-            // Check lower neighbor (if valid)
-            if (num - dist > 0 && isBinaryPalindrome(num - dist)) {
-                return dist;
-            }
-            // Check upper neighbor
-            if (isBinaryPalindrome(num + dist)) {
-                return dist;
-            }
-            dist++;
+function toBinary(x: number): string {
+    let s = "";
+    while (x) {
+        s += (x % 2).toString();
+        x = Math.floor(x / 2);
+    }
+    return s;
+}
+
+function isPalindrome(x: number): boolean {
+    const bin = toBinary(x);
+    const rev = bin.split("").reverse().join("");
+    return bin === rev;
+}
+
+function minOperations(nums: number[]): number[] {
+    const palindromes: number[] = [];
+    for (let i = 1; i <= 5000; i++) {
+        if (isPalindrome(i)) palindromes.push(i);
+    }
+
+    const ans: number[] = [];
+    for (const num of nums) {
+        let closest = 5000;
+        let l = 0, r = palindromes.length;
+        while (l < r) {
+            const m = (l + r) >> 1;
+            if (palindromes[m] <= num) l = m + 1;
+            else r = m;
         }
-    };
-    
-    return nums.map(findMinOps);
-};
+
+        if (l < palindromes.length) closest = palindromes[l] - num;
+        if (l > 0) closest = Math.min(closest, num - palindromes[l - 1]);
+        ans.push(closest);
+    }
+    return ans;
+}

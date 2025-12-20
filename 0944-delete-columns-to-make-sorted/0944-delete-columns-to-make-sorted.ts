511d055181da225961ed6334363b90e5ef5b2119
Richard Fu <44884321+furic@users.noreply.github.com>
2025-12-20 20:56:11 +1100

    Time: 6 ms (72.22%), Space: 58.9 MB (83.33%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.ts
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.ts
@@ -1,14 +1,21 @@
-function minDeletionSize(strs: string[]): number {
-    let result = 0;
+/**
+ * Counts columns that are not sorted lexicographically
+ * A column is sorted if characters read top-to-bottom are in non-decreasing order
+ */
+const minDeletionSize = (strs: string[]): number => {
+    let unsortedColumnCount = 0;
 
-    for(let i = 0; i < strs[0].length; i++) {
-        for(let j = 1; j < strs.length; j++) {
-            if(strs[j][i] < strs[j - 1][i]) {
-                result++;
-                break;
+    // Check each column
+    for (let col = 0; col < strs[0].length; col++) {
+        // Check if this column is sorted by comparing adjacent rows
+        for (let row = 1; row < strs.length; row++) {
+            // If current row's character is less than previous row's, column is unsorted
+            if (strs[row][col] < strs[row - 1][col]) {
+                unsortedColumnCount++;
+                break; // No need to check remaining rows in this column
             }
         }
     }
 
-    return result;
+    return unsortedColumnCount;
 };

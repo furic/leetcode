28ecbc525e3c2e746217ccff0c2e6b49a0092b06
Richard Fu <44884321+furic@users.noreply.github.com>
2025-12-26 04:13:32 +1100

    Time: 226 ms (66.67%), Space: 73.2 MB (100%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.ts
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.ts
@@ -1,11 +1,41 @@
 function maximumHappinessSum(happiness: number[], k: number): number {
-    happiness.sort((a, b) => b - a);
-    let total = 0;
-    for (let i = 0; i < k; i++){
-        let val = happiness[i] - i;
-        if (val > 0){
-            total += val;
+
+    // First thing is sort
+    // Grab the max one
+    // Then grab in order until complete
+    // Keep track of rounds, so that we don't get in the negative
+    // happiness side
+
+
+    let currHappiness = 0;
+
+    happiness.sort((a, b) => a - b);
+
+    let rounds = 0;
+
+    for (let i = happiness.length - 1; i >= 0; i--) {
+
+        // Substract round
+
+        const thisHappiness = happiness[i] - rounds;
+
+        if (thisHappiness <= 0) {
+            break;
         }
+
+        currHappiness += thisHappiness;
+
+        rounds++;
+
+        if (rounds === k) {
+            break;
+        }
+
     }
-    return total;
+
+
+
+    return currHappiness;
+
+
 };

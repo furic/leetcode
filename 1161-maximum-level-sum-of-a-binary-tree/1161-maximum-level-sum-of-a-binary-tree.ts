8f4f818b62de94b3e79ebd7592817b4376440cf3
Richard Fu <44884321+furic@users.noreply.github.com>
2026-01-06 18:21:06 +1100

    Time: 4 ms (98.68%), Space: 76.4 MB (9.27%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.ts
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.ts
@@ -1,24 +1,41 @@
-// 3 ms, array
-const maxLevelSum = (root) => {
-    const q = [root];
-    let i = 0;
-    let lvl = 1;
-    let maxLvl = 1;
-    let maxSum = root.val;
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
 
-    for (; i < q.length; ++lvl) {
-        const endLvl = q.length;
-        let sum = 0;
-        for (; i < endLvl; ++i) {
-            const node = q[i];
-            sum += node.val;
-            if (node.left) q.push(node.left);
-            if (node.right) q.push(node.right);
+function maxLevelSum(root: TreeNode | null): number {
+    let max: number = Number.MIN_SAFE_INTEGER;
+    let maxLevel: number = 1;
+    let queue: TreeNode[] = [root];
+    let index: number = 0;
+    let level: number = 0;
+    while ( index < queue.length ) {
+        const size: number = queue.length - index;
+        let levelsum: number = 0;
+        level++;
+        for ( let i = 0; i < size; i++ ) {
+            const curr = queue[index++];
+            levelsum += curr.val;
+            if ( curr.left ) {
+                queue.push(curr.left);
+            }
+            if ( curr.right ) {
+                queue.push(curr.right);
+            }
         }
-        if (sum > maxSum) {
-            maxSum = sum;
-            maxLvl = lvl;
+        if ( levelsum > max ) {
+            max = levelsum;
+            maxLevel = level;
         }
     }
-    return maxLvl;
+    return maxLevel;
 };

367a334d65fbdf47c43da29e2e1e062eae11fada
Richard Fu <44884321+furic@users.noreply.github.com>
2026-01-11 02:24:16 +1100

    Time: 25 ms (76.19%), Space: 59.7 MB (100%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.ts
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.ts
@@ -1,24 +1,30 @@
 function minimumDeleteSum(s1: string, s2: string): number {
-    let n1 = s1.length, n2 = s2.length;
-    let dp: number[] = Array(n2 + 1).fill(0);
+    const n = s1.length
+    const m = s2.length
+    const dp = Array(m + 1).fill(Infinity)
+    const prev = Array(m + 1).fill(Infinity)
+    
+    prev[0] = 0
+    dp[0] = 0
 
-    for (let i = 1; i <= n1; i++) {
-        let dp_new = [...dp];
-        for (let j = 1; j <= n2; j++) {
-            if (s1[i - 1] === s2[j - 1]) {
-                dp_new[j] = s2.charCodeAt(j - 1) + dp[j - 1];
-            } else {
-                dp_new[j] = Math.max(dp[j], dp_new[j - 1]);
-            }
-        }
-        dp = dp_new;
+    for (let j = 1; j <= m; j++) {
+        prev[j] = prev[j - 1] + s2.charCodeAt(j - 1)
     }
 
-    let total = 0;
-    for (let c of s1) 
-        total += c.charCodeAt(0);
-    for (let c of s2) 
-        total += c.charCodeAt(0);
+    for (let i = 1; i <= n; i++) {
+        dp[0] += s1.charCodeAt(i - 1)
+        for (let j = 1; j <= m; j++) {
+            if (s1[i - 1] === s2[j - 1]) {
+                dp[j] = prev[j - 1]
+            } else {
+                dp[j] = Math.min(dp[j - 1] + s2.charCodeAt(j - 1), dp[j] + s1.charCodeAt(i - 1))
+            }
+        }
 
-    return total - 2 * dp[n2];
+        for (let j = 0; j <= m; j++) {
+            prev[j] = dp[j]
+        }
+    }
+
+    return dp[m]
 };

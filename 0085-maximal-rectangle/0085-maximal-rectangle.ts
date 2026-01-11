f56d536853e16e78d8e04634cd1e1b437e11a378
Richard Fu <44884321+furic@users.noreply.github.com>
2026-01-11 15:41:37 +1100

    Time: 9 ms (83.08%), Space: 60.3 MB (58.46%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.ts
+++ 0085-maximal-rectangle/0085-maximal-rectangle.ts
@@ -1,23 +1,35 @@
-function maximalRectangle(matrix: string[][]): number {
-     if (matrix.length === 0) return 0;
-    let n = matrix[0].length;
-    let height = Array(n).fill(0);
-    let ans = 0;
+function largestRectangleArea(heights: number[]): number {
+    let maxArea = 0;
+    const stack: number[] = [];
 
-    for (let row of matrix) {
-        for (let i = 0; i < n; i++)
-            height[i] = row[i] === '1' ? height[i] + 1 : 0;
-
-        let stack = [];
-        for (let i = 0; i <= n; i++) {
-            let cur = i === n ? 0 : height[i];
-            while (stack.length && height[stack[stack.length-1]] > cur) {
-                let h = height[stack.pop()];
-                let w = stack.length === 0 ? i : i - stack[stack.length-1] - 1;
-                ans = Math.max(ans, h * w);
-            }
-            stack.push(i);
+    for (let i = 0; i <= heights.length; ++i) {
+        while (stack.length && (i === heights.length || heights[i] < heights[stack[stack.length - 1]])) {
+            const height = heights[stack.pop()!];
+            const width = stack.length ? i - stack[stack.length - 1] - 1 : i;
+            maxArea = Math.max(maxArea, height * width);
         }
+        stack.push(i);
     }
-    return ans;
-};
+    return maxArea;
+}
+
+function maximalRectangle(matrix: string[][]): number {
+    if (!matrix || !matrix[0]) return 0;
+
+    const rows = matrix.length, cols = matrix[0].length;
+    let maxArea = 0;
+    const heights = new Array(cols).fill(0);
+
+    for (let row = 0; row < rows; ++row) {
+        for (let col = 0; col < cols; ++col) {
+            if (matrix[row][col] === '1') {
+                heights[col] += 1;
+            } else {
+                heights[col] = 0;
+            }
+        }
+        maxArea = Math.max(maxArea, largestRectangleArea(heights));
+    }
+
+    return maxArea;
+}

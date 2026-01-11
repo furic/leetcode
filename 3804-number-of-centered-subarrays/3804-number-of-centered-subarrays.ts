52483687d121b82781b7a0865ce7e946ed58d2dd
Richard Fu <44884321+furic@users.noreply.github.com>
2026-01-11 15:14:42 +1100

    Time: 60 ms (100%), Space: 62.7 MB (11.11%) - LeetHub

--- 3804-number-of-centered-subarrays/3804-number-of-centered-subarrays.ts
+++ 3804-number-of-centered-subarrays/3804-number-of-centered-subarrays.ts
@@ -1,19 +1,20 @@
 const centeredSubarrays = (nums: number[]): number => {
     const n = nums.length;
     let count = 0;
-
+    
     for (let i = 0; i < n; i++) {
         const elements = new Set<number>();
         let sum = 0;
-
+        
         for (let j = i; j < n; j++) {
             sum += nums[j];
             elements.add(nums[j]);
-
+            
             if (elements.has(sum)) {
                 count++;
             }
         }
     }
+    
     return count;
 };

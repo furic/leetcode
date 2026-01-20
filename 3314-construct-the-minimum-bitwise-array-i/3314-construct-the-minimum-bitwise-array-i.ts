e94688f9b84db80514de496b3dab15b1ec46ae73
Richard Fu <44884321+furic@users.noreply.github.com>
2026-01-21 02:06:20 +1100

    Time: 1 ms (86.67%), Space: 58 MB (80%) - LeetHub

---
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.ts
@@ -0,0 +1,9 @@
+function minBitwiseArray(nums: number[]): number[] {
+      const ans = []
+    for(let i = 0; i < nums.length; i++) {
+        let n = nums[i]
+        if(n != 2) ans[i] = n - ((n + 1) & (-n - 1)) / 2
+        else ans[i] = -1
+    }  
+    return ans
+};

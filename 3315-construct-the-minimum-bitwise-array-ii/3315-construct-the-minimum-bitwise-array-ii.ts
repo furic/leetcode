3338b131b5872c649bc994e17500472dd8925eec
Richard Fu <44884321+furic@users.noreply.github.com>
2026-01-21 20:33:15 +1100

    Time: 1 ms (0%), Space: 58.4 MB (100%) - LeetHub

--- 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.ts
+++ 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.ts
@@ -1,13 +1,17 @@
-function minBitwiseArray(nums: number[]): number[] {
-        for (let i = 0; i < nums.length; i++) {
-        let x = nums[i];
-        let res = -1;
-        let d = 1;
-        while ((x & d) !== 0) {
-            res = x - d;
-            d <<= 1;
+const minBitwiseArray = (nums: number[]): number[] => {
+    for (let i = 0; i < nums.length; i++) {
+        const prime = nums[i];
+        let answer = -1;
+        let bitValue = 1;
+        
+        // Find the first 0-bit position (from right) and subtract it from prime
+        // This gives us the minimum value where value OR (value + 1) === prime
+        while ((prime & bitValue) !== 0) {
+            answer = prime - bitValue;
+            bitValue <<= 1;
         }
-        nums[i] = res;
+        
+        nums[i] = answer;
     }
     return nums;
 };

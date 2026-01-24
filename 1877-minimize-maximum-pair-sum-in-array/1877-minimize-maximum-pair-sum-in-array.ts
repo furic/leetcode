2cbd4821420ca7fa730440c638125a43160870e6
Richard Fu <44884321+furic@users.noreply.github.com>
2026-01-25 01:52:06 +1100

    Time: 269 ms (33.33%), Space: 70.6 MB (55.56%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.ts
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.ts
@@ -1,10 +1,9 @@
 function minPairSum(nums: number[]): number {
-    nums.sort((a, b) => a - b);
-    let i = 0, j = nums.length - 1, ans = 0;
-    while (i < j) {
-        ans = Math.max(ans, nums[i] + nums[j]);
-        i++;
-        j--;
+    nums.sort((a, b)=>a-b);
+    const n = nums.length;
+    let result = Number.MIN_SAFE_INTEGER;
+    for(let i=0;i< n/2;i++){
+         result = Math.max(result, nums[i] + nums[n - i - 1]);
     }
-    return ans;
-}
+    return result;
+};

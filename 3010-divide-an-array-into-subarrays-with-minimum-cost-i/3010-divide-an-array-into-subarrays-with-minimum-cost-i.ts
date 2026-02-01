e5fd434a21761f785706b347a300ca53952d841c
Richard Fu <44884321+furic@users.noreply.github.com>
2026-02-01 13:23:35 +1100

    Time: 1 ms (77.78%), Space: 58.4 MB (11.11%) - LeetHub

--- 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.ts
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.ts
@@ -1,9 +1,28 @@
-function minimumCost(nums: number[]) {
-    const first = nums[0], min = Math.min;
-    let min1 = Infinity, min2 = min1;
-    for (let i = 1, n = nums.length; i < n; i++) {
-        if (nums[i] < min2) min1 = min2, min2 = nums[i];
-        else if (nums[i] < min1) min1 = nums[i];
+/**
+ * Finds minimum cost to divide array into 3 contiguous subarrays
+ * Cost of subarray = its first element
+ * Strategy: First subarray cost is fixed (nums[0]), find two smallest remaining elements
+ */
+const minimumCost = (nums: number[]): number => {
+    const firstSubarrayCost = nums[0];
+    
+    // Find the two smallest values in nums[1..n-1]
+    // These will be the first elements of the second and third subarrays
+    let secondSmallest = Infinity;
+    let smallest = Infinity;
+    
+    for (let index = 1; index < nums.length; index++) {
+        const currentValue = nums[index];
+        
+        if (currentValue < smallest) {
+            // Found new smallest: shift current smallest to second smallest
+            secondSmallest = smallest;
+            smallest = currentValue;
+        } else if (currentValue < secondSmallest) {
+            // Found new second smallest (but not smaller than smallest)
+            secondSmallest = currentValue;
+        }
     }
-    return first + min1 + min2;
-}
+    
+    return firstSubarrayCost + smallest + secondSmallest;
+};

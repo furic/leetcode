b9535a51f9fbb51563be99671306ae1a10ee6e65
Richard Fu <44884321+furic@users.noreply.github.com>
2026-02-05 23:07:01 +1100

    Time: 56 ms (58.33%), Space: 59 MB (8.33%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.ts
+++ 3379-transformed-array/3379-transformed-array.ts
@@ -1,8 +1,14 @@
 function constructTransformedArray(nums: number[]): number[] {
-    const n = nums.length;
-    const ans = new Array(n);
-    for (let i = 0; i < n; i++) {
-        ans[i] = nums[((i + nums[i]) % n + n) % n];
+    const n = nums.length
+    const result = Array(n)
+    for(let i = 0; i < n; i++) {
+        if(nums[i] > 0) {
+            result[i] = nums[(i + nums[i]) % n]
+        } else if(nums[i] === 0) {
+            result[i] = nums[i]
+        } else if(nums[i] < 0) {
+            result[i] = nums[((i + nums[i]) % n + n) % n]
+        }
     }
-    return ans;
+    return result
 };

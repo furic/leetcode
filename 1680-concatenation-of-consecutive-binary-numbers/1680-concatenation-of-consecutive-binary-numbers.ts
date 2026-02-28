343fda6a60d0fd29b975e784a81544461b9ccd8f
Richard Fu <44884321+furic@users.noreply.github.com>
2026-03-01 01:26:21 +1100

    Time: 750 ms (100%), Space: 61.9 MB (100%) - LeetHub

---
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.ts
@@ -0,0 +1,11 @@
+function concatenatedBinary(n: number): number {
+    const MOD = 1000000007n;
+    let res = 0n;
+
+    for (let i = 1n; i <= BigInt(n); i++) {
+        const bits = i.toString(2).length;
+        res = ((res << BigInt(bits)) + i) % MOD;
+    }
+
+    return Number(res);
+};

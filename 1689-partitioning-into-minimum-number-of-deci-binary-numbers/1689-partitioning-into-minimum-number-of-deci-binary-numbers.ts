c3478e0bdf27de5d0a64508c4c2208b5ed91f5c1
Richard Fu <44884321+furic@users.noreply.github.com>
2026-03-01 18:27:45 +1100

    Time: 33 ms (10.34%), Space: 65.3 MB (10.34%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.ts
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.ts
@@ -1,6 +1,6 @@
+// The largest digit determines how many deci-binary numbers are needed,
+// since each position can only contribute 0 or 1 per number.
 const minPartitions = (n: string): number => {
-    // The largest digit determines how many deci-binary numbers are needed,
-    // since each position can only contribute 0 or 1 per number.
     const largestDigit = Math.max(...n.split('').map(Number));
     return largestDigit;
 };

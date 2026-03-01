1fe09786b4a55d20c677c096909506c76cb104d3
Richard Fu <44884321+furic@users.noreply.github.com>
2026-03-01 17:14:01 +1100

    Time: 1 ms (53.57%), Space: 57.7 MB (92.86%) - LeetHub

--- 3856-trim-trailing-vowels/3856-trim-trailing-vowels.ts
+++ 3856-trim-trailing-vowels/3856-trim-trailing-vowels.ts
@@ -1,7 +1,7 @@
 const trimTrailingVowels = (s: string): string => {
     const VOWELS = 'aeiou';
     let i = s.length - 1;
-    while(i >= 0 && VOWELS.includes(s[i])) {
+    while (i >= 0 && VOWELS.includes(s[i])) {
         i--;
     }
     return s.substring(0, i + 1);

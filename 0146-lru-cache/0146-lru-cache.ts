ba8144631183b1769888c35c142db7986e2a472a
Richard Fu <44884321+furic@users.noreply.github.com>
2026-03-11 20:08:13 +1100

    Time: 89 ms (93.78%), Space: 113.8 MB (6.44%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.ts
+++ 0146-lru-cache/0146-lru-cache.ts
@@ -1,32 +1,132 @@
-class LRUCache {
-    private readonly capacity: number;
-    private readonly cache: Map<number, number>;
+class Node {
+    value: number;
+    key: number;
+    next: Node | null;
+    prev: Node | null;
 
+    constructor(k: number, v: number){
+        this.value = v;
+        this.key = k;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DLL {
+    capacity: number;
+    head: Node | null;
+    tail: Node | null;
+    length: number;
+    map: Map<number, Node>;
+
+    constructor(c: number){
+        this.capacity = c
+        this.length = 0
+        this.map = new Map()
+    }
+
+    add(k: number, v: number){
+        if(this.map.has(k)){
+            const r = this.get(k);
+            this.tail.value = v;
+            return r
+        }
+
+        const node = new Node(k, v);
+
+        if(this.length === this.capacity) {
+            this.setMostRecent(node)
+            this.removeLestRecentNode()
+        } else {
+            if(this.length === 0){
+                this.tail = node;
+                this.head = node;
+            } else {
+                this.setMostRecent(node)
+            }
+        }
+        this.map.set(k, node)
+        this.length = this.length + 1;
+    }
+
+    setMostRecent(n: Node){
+        n.prev = this.tail;
+        this.tail.next = n;
+        this.tail = n;
+    }
+
+    removeLestRecentNode(){
+        const n = this.head;
+
+        if(this.map.has(n.key)){
+            this.map.delete(n.key);
+        }
+
+        this.head = this.head.next;
+        if(this.head){
+            this.head.prev = null;
+        }
+        
+        // Delete
+        n.prev = null;
+        n.next = null;
+
+        this.length = this.length - 1;
+    }
+
+    get(k: number): number {
+        if(!this.map.has(k)) return -1
+        if(this.length === 1) return this.tail.value;
+
+        const node = this.map.get(k)
+        // The first one goes to last
+        if(node.key === this.head.key) {
+            this.head = this.head.next;
+            this.head.prev = null;
+
+            node.prev = this.tail;
+            this.tail.next = node;
+            node.next = null;
+            
+            this.tail = node;
+        } else if(node.key === this.tail.key) {
+            return this.tail.value
+        } else {
+            const prev = node.prev
+            const next = node.next
+            // Connect cables in the middle
+            prev.next = next
+            next.prev = prev
+
+            //Update tail
+            node.prev = this.tail
+            this.tail.next = node
+            node.next = null
+            this.tail = node
+        }
+        
+        return node.value
+    }
+}
+
+class LRUCache {
+    DLL: DLL
     constructor(capacity: number) {
-        this.capacity = capacity;
-        this.cache = new Map();
+        this.DLL = new DLL(capacity)
     }
 
     get(key: number): number {
-        const value = this.cache.get(key);
-        if (value === undefined) return -1;
-
-        // Refresh recency: delete and re-insert to move key to Map's insertion-order tail
-        this.cache.delete(key);
-        this.cache.set(key, value);
-
-        return value;
+        return this.DLL.get(key)
     }
 
     put(key: number, value: number): void {
-        // Evict least recently used (Map's insertion-order head) only when adding a new key
-        if (!this.cache.has(key) && this.cache.size >= this.capacity) {
-            const lruKey = this.cache.keys().next().value!;
-            this.cache.delete(lruKey);
-        }
-
-        // Refresh recency same as get: delete then re-insert places key at tail
-        this.cache.delete(key);
-        this.cache.set(key, value);
+        this.DLL.add(key, value)
     }
 }
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */

1437ade3ebf0528a4a29cb7be3eec5017d4f0dd6
Richard Fu <44884321+furic@users.noreply.github.com>
2026-03-13 21:45:35 +1100

    Time: 9 ms (61.27%), Space: 66.4 MB (41.04%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.ts
+++ 0056-merge-intervals/0056-merge-intervals.ts
@@ -1,20 +1,23 @@
 function merge(intervals: number[][]): number[][] {
-    intervals.sort((a, b) => a[0] - b[0]);
-    let merged: number[][] = [];
-    for (let interval of intervals) {
-        // if the list of merged intervals is empty or if the current
-        // interval does not overlap with the previous, append it
-        if (merged.length === 0 || merged[merged.length - 1][1] < interval[0]) {
-            merged.push(interval);
-        }
-        // otherwise, there is overlap, so we merge the current and previous
-        // intervals
-        else {
-            merged[merged.length - 1][1] = Math.max(
-                merged[merged.length - 1][1],
-                interval[1],
-            );
-        }
+    // [1     3]
+    //    [2       6]
+    // If cur end is > next_start, combine
+    intervals.sort((a, b) => a[0] - b[0])
+    if (intervals.length === 0 || intervals.length === 1) return intervals
+    const new_intervals = []
+    let curr_interval = intervals[0]
+    
+    for (let i = 1; i < intervals.length; i++) {
+        const next_interval = intervals[i]
+        const current_end = curr_interval[1]
+        const next_start = next_interval[0]
+        if (current_end >= next_start) {
+            curr_interval[1] = Math.max(next_interval[1], curr_interval[1])
+        } else  {
+            new_intervals.push(curr_interval)
+            curr_interval = next_interval
+        } 
     }
-    return merged;
-}
+    new_intervals.push(curr_interval)
+    return new_intervals
+};

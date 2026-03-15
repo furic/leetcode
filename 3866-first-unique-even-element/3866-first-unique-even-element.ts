93e5c67e30ef3d583ea4cc9813e74c5da0482d43
Richard Fu <44884321+furic@users.noreply.github.com>
2026-03-15 14:31:40 +1100

    Time: 3 ms (100%), Space: 58.6 MB (100%) - LeetHub

--- 3866-first-unique-even-element/3866-first-unique-even-element.ts
+++ 3866-first-unique-even-element/3866-first-unique-even-element.ts
@@ -1,19 +1,5 @@
-const firstUniqueEven = (nums: number[]): number => {
-    const evens = new Set<number>();
-    const duplicates = new Set<number>();
-    for (let num of nums) {
-        if (duplicates.has(num)) continue;
-        if (num % 2 === 0) {
-            if (evens.has(num)) {
-                evens.delete(num);
-                duplicates.add(num);
-            } else {
-                evens.add(num)
-            }
-        }
-    }
-    for (let num of nums) {
-        if (evens.has(num)) return num;
-    }
-    return -1;
-};
+function firstUniqueEven(nums: number[]): number {
+  const cnt = new Map<number, number>();
+  for (const x of nums) cnt.set(x, (cnt.get(x) ?? 0) + 1);
+  return nums.find(x => x % 2 === 0 && cnt.get(x) === 1) ?? -1;
+}

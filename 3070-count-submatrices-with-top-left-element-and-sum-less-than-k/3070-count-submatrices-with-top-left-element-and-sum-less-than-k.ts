f1325864d86d1b3cb1d46dec905e879677aff528
Richard Fu <44884321+furic@users.noreply.github.com>
2026-03-18 18:27:41 +1100

    Time: 5 ms (50%), Space: 74 MB (0%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.ts
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.ts
@@ -1,32 +1,19 @@
 function countSubmatrices(grid: number[][], k: number): number {
-    let n = grid.length;
-    let m = grid[0].length;
-    let cnt = 0;
-    if(grid[0][0] <= k){
-        cnt += 1;
-    }else{
-        return 0;
-    }
+    const n: number = grid.length;
+    const m: number = grid[0].length;
+    const cols: number[] = new Array(m).fill(0);
+    let res: number = 0;
 
-    for(let i = 1; i < n; i++){
-        grid[i][0] += grid[i - 1][0]
-        if(grid[i][0] <= k){
-            cnt += 1;
-        }
-    }
-    for(let j = 1; j < m; j++){
-        grid[0][j] += grid[0][j - 1]
-        if(grid[0][j] <= k){
-            cnt += 1
-        }
-    }
-    for(let i = 1; i < n; i++){
-        for(let j = 1; j < m; j++){
-            grid[i][j] += grid[i - 1][j] + grid[i][j - 1] - grid[i - 1][j - 1]
-            if(grid[i][j] <= k){
-                cnt += 1
+    for (let i = 0; i < n; i++) {
+        let rows: number = 0;
+        for (let j = 0; j < m; j++) {
+            cols[j] += grid[i][j];
+            rows += cols[j];
+            if (rows <= k) {
+                res++;
             }
         }
     }
-    return cnt;
-};
+
+    return res;
+}

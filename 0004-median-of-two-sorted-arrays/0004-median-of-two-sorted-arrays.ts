cf30850f74777d79713f1faafd3d633cb7fbf87d
Richard Fu <44884321+furic@users.noreply.github.com>
2026-03-19 19:09:50 +1100

    Time: 2 ms (89.27%), Space: 58.8 MB (84.28%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.ts
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.ts
@@ -1,42 +1,32 @@
-function findMedianSortedArrays(nums1: number[], nums2: number[]):
-  number {
-      // Ensure binary search on the shorter array
-      if (nums1.length > nums2.length) {
-          const tmp = nums1; nums1 = nums2; nums2 = tmp;
-      }
+const findMedianSortedArrays = (nums1: number[], nums2: number[]): number => {
+    // Binary search on the shorter array
+    if (nums1.length > nums2.length) [nums1, nums2] = [nums2, nums1];
 
-      const m = nums1.length;
-      const n = nums2.length;
-      const half = (m + n + 1) >> 1;
+    const m = nums1.length;
+    const n = nums2.length;
+    const halfLen = (m + n + 1) >> 1;
 
-      let lo = 0;
-      let hi = m;
+    let lo = 0;
+    let hi = m;
 
-      while (lo <= hi) {
-          const i = (lo + hi) >> 1;      // partition in nums1
-          const j = half - i;             // partition in nums2
+    while (lo <= hi) {
+        const cut1 = (lo + hi) >> 1;       // partition index in nums1
+        const cut2 = halfLen - cut1;        // partition index in nums2
 
-          const left1  = i > 0 ? nums1[i - 1] : -Infinity;
-          const right1 = i < m ? nums1[i]     :  Infinity;
-          const left2  = j > 0 ? nums2[j - 1] : -Infinity;
-          const right2 = j < n ? nums2[j]     :  Infinity;
+        const left1  = cut1 > 0 ? nums1[cut1 - 1] : -Infinity;
+        const right1 = cut1 < m ? nums1[cut1]      :  Infinity;
+        const left2  = cut2 > 0 ? nums2[cut2 - 1]  : -Infinity;
+        const right2 = cut2 < n ? nums2[cut2]       :  Infinity;
 
-          if (left1 <= right2 && left2 <= right1) {
-              // Found valid partition
-              if ((m + n) & 1) {
-                  return left1 > left2 ? left1 : left2;
-              }
-              const maxLeft = left1 > left2 ? left1 : left2;
-              const minRight = right1 < right2 ? right1 : right2;
-              return (maxLeft + minRight) / 2;
-          }
+        if (left1 <= right2 && left2 <= right1) {
+            const maxLeft  = Math.max(left1, left2);
+            const minRight = Math.min(right1, right2);
+            return (m + n) & 1 ? maxLeft : (maxLeft + minRight) / 2;
+        }
 
-          if (left1 > right2) {
-              hi = i - 1;
-          } else {
-              lo = i + 1;
-          }
-      }
+        if (left1 > right2) hi = cut1 - 1;
+        else                lo = cut1 + 1;
+    }
 
-      return 0;
-  }
+    return 0;
+};

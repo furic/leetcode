d02520a50453496389678cb661611615115c0f4b
Richard Fu <44884321+furic@users.noreply.github.com>
2026-03-20 21:25:48 +1100

    Time: 28 ms (68.05%), Space: 67.7 MB (76.89%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.ts
+++ 0049-group-anagrams/0049-group-anagrams.ts
@@ -1,4 +1,4 @@
-function groupAnagrams(strs: string[]): string[][] {
+const groupAnagrams = (strs: string[]): string[][] => {
     const anagramsMap = new Map<string, string[]>();
     strs.forEach((str) => {
         const key = str.split('').sort().join();

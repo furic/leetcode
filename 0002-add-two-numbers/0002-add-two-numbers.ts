6c2ba9c625a6f88d420e01b842e55774f8758b25
Richard Fu <44884321+furic@users.noreply.github.com>
2026-04-04 21:39:29 +1100

    Time: 6 ms (22.81%), Space: 62.5 MB (36.81%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.ts
+++ 0002-add-two-numbers/0002-add-two-numbers.ts
@@ -10,45 +10,20 @@
  * }
  */
 
-function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-
-    let result: ListNode | null = null
-    let current: ListNode | null = null
-
-    let p1: ListNode | null = l1
-    let p2: ListNode | null = l2
-
-    let buffer: number = 0
-
-    while(p1 !== null || p2 !== null || buffer !== 0) {
-        const val1: number = p1 === null ? 0 : p1.val
-        const val2: number = p2 === null ? 0 : p2.val
-
-        let sum: number = val1 + val2 + buffer
-        if(sum > 9) {
-            buffer = 1
-            sum = sum - 10
-        } else {
-            buffer = 0
-        }
-        let newNode: ListNode = new ListNode(sum, null)
-
-        if(result === null) {
-            result = newNode
-        }
-
-        if(current === null) {
-            current = newNode
-        } else {
-            current.next = newNode
-            current = newNode
-        }
-
-
-        p1 = p1?.next ?? null
-        p2 = p2?.next ?? null
+const addTwoNumbers = (l1: ListNode | null, l2: ListNode | null): ListNode | null => {
+    let dummyHead = new ListNode();
+    let current = dummyHead;
+    let carry = 0;
+    while (l1 || l2) {
+        const sum = carry + (l1?.val ?? 0) + (l2?.val ?? 0);
+        current.next = new ListNode(sum % 10);
+        carry = Math.floor(sum / 10);
+        current = current.next;
+        l1 = l1?.next;
+        l2 = l2?.next;
     }
-
-
-    return result
-}
+    if (carry) {
+        current.next = new ListNode(carry);
+    }
+    return dummyHead.next;
+};

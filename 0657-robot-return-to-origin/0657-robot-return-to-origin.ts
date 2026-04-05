afb64f53ea7728af1c1bf54c08414e0e84bfb9ce
Richard Fu <44884321+furic@users.noreply.github.com>
2026-04-05 12:17:14 +1000

    Time: 4 ms (82.61%), Space: 58.6 MB (44.56%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.ts
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.ts
@@ -1,12 +1,12 @@
-function judgeCircle(moves: string): boolean {
+const judgeCircle = (moves: string): boolean => {
     let x = 0, y = 0;
 
-    for (const m of moves) {
-        if (m === 'R') x++;
-        else if (m === 'L') x--;
-        else if (m === 'U') y++;
-        else if (m === 'D') y--;
+    for (const move of moves) {
+        if      (move === 'R') x++;
+        else if (move === 'L') x--;
+        else if (move === 'U') y++;
+        else if (move === 'D') y--;
     }
 
     return x === 0 && y === 0;
-}
+};

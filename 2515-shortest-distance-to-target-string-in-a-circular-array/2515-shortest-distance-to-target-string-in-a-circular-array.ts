795d8effbdc0e0f46c6db98ad4c806c91d012f02
Richard Fu <44884321+furic@users.noreply.github.com>
2026-04-15 18:19:40 +1000

    Time: 0 ms (100%), Space: 58.5 MB (100%) - LeetHub

---
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.ts
@@ -0,0 +1,17 @@
+function closestTarget(
+    words: string[],
+    target: string,
+    startIndex: number,
+): number {
+    let ans = words.length;
+    const n = words.length;
+
+    for (let i = 0; i < n; ++i) {
+        if (words[i] === target) {
+            const dist = Math.abs(i - startIndex);
+            ans = Math.min(ans, Math.min(dist, n - dist));
+        }
+    }
+
+    return ans < n ? ans : -1;
+}

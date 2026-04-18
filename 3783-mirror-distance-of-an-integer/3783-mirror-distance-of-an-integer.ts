05a12d2a00e5606fffc9ce2bfc620216d32fd7f9
Richard Fu <44884321+furic@users.noreply.github.com>
2026-04-19 01:32:02 +1000

    Time: 2 ms (23.68%), Space: 57.7 MB (23.68%) - LeetHub

--- 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.ts
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.ts
@@ -1,11 +1,2 @@
-function mirrorDistance(n: number): number {
-    // return (n - Number(n.toString())) * -1
-
-    let numArr = n.toString().split("")
-    let reverseArr = []
-    for (let i = numArr.length; i > 0; i--) {
-        reverseArr.push(numArr[i-1])
-    }
-
-    return Math.abs(n - Number(reverseArr.join("")))
-};
+const mirrorDistance = (n: number): number =>
+    Math.abs(n - +n.toString().split('').reverse().join(''));

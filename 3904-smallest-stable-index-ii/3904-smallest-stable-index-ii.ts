aa9a6381164419424188d1b5bf478ec01e9d246a
Richard Fu <44884321+furic@users.noreply.github.com>
2026-04-20 00:10:34 +1000

    Time: 14 ms (76.19%), Space: 77.5 MB (95.24%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.ts
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.ts
@@ -1,21 +1,15 @@
-function findMin(nums: number[], startIndex: number) {
-    let min = nums[startIndex];
-    let minIndex = startIndex;
-    for(let i = startIndex; i < nums.length; i++) {
-        min = Math.min(min, nums[i]);
-        if(min === nums[i]) minIndex = i;
-    }
-    return minIndex;
-}
+const firstStableIndex = (nums: number[], k: number): number => {
+    const n = nums.length;
 
-function firstStableIndex(nums: number[], k: number): number {
-    let max = -1;
-    let minIndex = findMin(nums, 0);
-    for(let i = 0; i < nums.length; i++) {
-        if(i > minIndex) minIndex = findMin(nums, i);
-        max = Math.max(max, nums[i]);
-        const min = nums[minIndex];
-        if((max - min) <= k) return i;
+    const suffixMin = new Array(n);
+    suffixMin[n - 1] = nums[n - 1];
+    for (let i = n - 2; i >= 0; i--)
+        suffixMin[i] = Math.min(nums[i], suffixMin[i + 1]);
+
+    let prefixMax = -Infinity;
+    for (let i = 0; i < n; i++) {
+        prefixMax = Math.max(prefixMax, nums[i]);
+        if (prefixMax - suffixMin[i] <= k) return i;
     }
 
     return -1;

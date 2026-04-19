5214ad10052a1d86cce65331ae626bdd568d52cd
Richard Fu <44884321+furic@users.noreply.github.com>
2026-04-20 00:48:22 +1000

    Time: 4 ms (53.33%), Space: 68.7 MB (26.67%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.ts
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.ts
@@ -1,17 +1,12 @@
-function maxDistance(nums1: number[], nums2: number[]): number {
-    let i = 0;
-    let res = 0;
-    const n1 = nums1.length, n2 = nums2.length;
+const maxDistance = (nums1: number[], nums2: number[]): number => {
+    let left = 0;
+    let maxDist = 0;
 
-    for (let j = 0; j < n2; j++) {
-        while (i < n1 && nums1[i] > nums2[j]) {
-            i++;
-        }
-        if (i === n1) 
-            break;
-
-        res = Math.max(res, j - i);
+    for (let right = 0; right < nums2.length; right++) {
+        while (left < nums1.length && nums1[left] > nums2[right]) left++;
+        if (left === nums1.length) break;
+        maxDist = Math.max(maxDist, right - left);
     }
 
-    return res;
+    return maxDist;
 };

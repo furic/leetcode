43f8a4bef5a1f1f069177c5eeb88e47813cd2b11
Richard Fu <44884321+furic@users.noreply.github.com>
2026-04-20 00:13:32 +1000

    Time: 5 ms (95.24%), Space: 74.2 MB (100%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.ts
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.ts
@@ -1,3 +1,4 @@
+/*
 function findMin(nums: number[], startIndex: number) {
     let min = nums[startIndex];
     let minIndex = startIndex;
@@ -20,3 +21,39 @@ function firstStableIndex(nums: number[], k: number): number {
 
     return -1;
 };
+*/
+
+function findSuffixMinIdx(nums: number[], startIndex: number) {
+    let min = nums[startIndex];
+    let minIndex = startIndex;
+    for(let i = startIndex; i < nums.length; i++) {
+        min = Math.min(min, nums[i]);
+        if(min === nums[i]) minIndex = i;
+    }
+    return minIndex;
+}
+
+// function findSuffixMinIdx(nums: number[], from: number): number {
+//     let min = nums[from];
+//     let minIdx = from;
+//     for (let i = from; i < nums.length; i++) {
+//         if (nums[i] < min) {
+//             min = nums[i];
+//             minIdx = i;
+//         }
+//     }
+//     return minIdx;
+// }
+
+function firstStableIndex(nums: number[], k: number): number {
+    let prefixMax = -1;
+    let suffixMinIdx = findSuffixMinIdx(nums, 0);
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i > suffixMinIdx) suffixMinIdx = findSuffixMinIdx(nums, i);
+        prefixMax = Math.max(prefixMax, nums[i]);
+        if (prefixMax - nums[suffixMinIdx] <= k) return i;
+    }
+
+    return -1;
+}

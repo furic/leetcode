ecb49d49a1e607ba72efa592cdcbba20eb3c2d55
Richard Fu <44884321+furic@users.noreply.github.com>
2026-04-22 18:16:58 +1000

    Time: 8 ms (60%), Space: 58.7 MB (30%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.ts
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.ts
@@ -1,23 +1,19 @@
-function twoEditWords(queries: string[], dictionary: string[]): string[] {
-  const result: string[] = [];
+const twoEditWords = (queries: string[], dictionary: string[]): string[] => {
+    const result: string[] = [];
 
-  for (const query of queries) {
-    for (const word of dictionary) {
-      let diffCount = 0;
-      for (let i = 0; i < query.length; i++) {
-        if (query[i] !== word[i]) {
-          diffCount++;
+    for (const query of queries) {
+        for (const word of dictionary) {
+            let diffCount = 0;
+            for (let i = 0; i < query.length; i++) {
+                if (query[i] !== word[i]) diffCount++;
+                if (diffCount > 2) break;
+            }
+            if (diffCount <= 2) {
+                result.push(query);
+                break;
+            }
         }
-        if (diffCount > 2) {
-          break;
-        }
-      }
-      if (diffCount <= 2) {
-        result.push(query);
-        break;
-      }
     }
-  }
 
-  return result;
-}
+    return result;
+};

3cb5f6ab62dec248b3c8972bb4aa15610442e8f1
Richard Fu <44884321+furic@users.noreply.github.com>
2026-04-24 21:32:54 +1000

    Time: 1 ms (81.25%), Space: 58.6 MB (21.88%) - LeetHub

--- 2833-furthest-point-from-origin/2833-furthest-point-from-origin.ts
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.ts
@@ -1,15 +1,15 @@
 function furthestDistanceFromOrigin(moves: string): number {
-    let L = 0, R = 0, B = 0;
+    let a = 0, B = 0;
 
     for (let c of moves) {
         if (c === 'L') {
-            L++;
+            a--;
         } else if (c === 'R') {
-            R++;
+            a++;
         } else {
             B++;
         }
     }
 
-    return Math.abs(L - R) + B;
+    return Math.abs(a) + B;
 }

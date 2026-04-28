f613a7be418529ad142ed54976286d28d1cd599d
Richard Fu <44884321+furic@users.noreply.github.com>
2026-04-28 21:07:33 +1000

    Time: 116 ms (100%), Space: 80.3 MB (83.33%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.ts
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.ts
@@ -1,9 +1,14 @@
 const minOperations = (grid: number[][], x: number): number => {
-    const values = grid.flat().sort((a, b) => a - b);
+    const values = grid.flat();
     const targetMod = values[0] % x;
 
-    if (values.some(v => v % x !== targetMod)) return -1;
+    for (const v of values)
+        if (v % x !== targetMod) return -1;
 
+    values.sort((a, b) => a - b);
     const median = values[Math.floor(values.length / 2)];
-    return values.reduce((ops, v) => ops + Math.abs(v - median) / x, 0);
+
+    let ops = 0;
+    for (const v of values) ops += Math.abs(v - median) / x;
+    return ops;
 };

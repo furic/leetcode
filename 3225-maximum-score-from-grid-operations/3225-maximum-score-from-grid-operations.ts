a9963a01682bae45a561c6dedc35bc345ad2f6d7
Richard Fu <44884321+furic@users.noreply.github.com>
2026-04-29 20:42:56 +1000

    Time: 93 ms (100%), Space: 64.7 MB (100%) - LeetHub

--- 3225-maximum-score-from-grid-operations/3225-maximum-score-from-grid-operations.ts
+++ 3225-maximum-score-from-grid-operations/3225-maximum-score-from-grid-operations.ts
@@ -1,29 +1,31 @@
-function maximumScore(grid: number[][]): number {
+const maximumScore = (grid: number[][]): number => {
     const n = grid.length;
-    if (n === 1) 
-        return 0;
+    if (n === 1) return 0;
+
     let dp0 = new Array(n + 1).fill(0);
     let dp1 = new Array(n + 1).fill(0);
 
-    for (let j = 1; j < n; j++) {
-        let new_dp0 = new Array(n + 1).fill(0);
-        let new_dp1 = new Array(n + 1).fill(0);
+    for (let col = 1; col < n; col++) {
+        const newDp0 = new Array(n + 1).fill(0);
+        const newDp1 = new Array(n + 1).fill(0);
 
         for (let i = 0; i <= n; i++) {
-            let prev = 0, curr = 0;
-            for (let x = 0; x < i; x++)
-                curr += grid[x][j];
-            for (let y = 0; y <= n; y++) {
-                if (y > 0 && y <= i)
-                    curr -= grid[y - 1][j];
-                if (y > i)
-                    prev += grid[y - 1][j - 1];
-                new_dp0[y] = Math.max(new_dp0[y], prev + dp0[i], dp1[i]);
-                new_dp1[y] = Math.max(new_dp1[y], curr + dp1[i], curr + prev + dp0[i]);
+            let prevColSum = 0;
+            let currColSum = 0;
+            for (let x = 0; x < i; x++) currColSum += grid[x][col];
+
+            for (let row = 0; row <= n; row++) {
+                if (row > 0 && row <= i) currColSum -= grid[row - 1][col];
+                if (row > i)             prevColSum += grid[row - 1][col - 1];
+
+                newDp0[row] = Math.max(newDp0[row], prevColSum + dp0[i], dp1[i]);
+                newDp1[row] = Math.max(newDp1[row], currColSum + dp1[i], currColSum + prevColSum + dp0[i]);
             }
         }
-        dp0 = new_dp0;
-        dp1 = new_dp1;
+
+        dp0 = newDp0;
+        dp1 = newDp1;
     }
+
     return Math.max(...dp1);
 };

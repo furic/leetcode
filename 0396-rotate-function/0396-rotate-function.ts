c0a7ab14f75a99ca3f3bfab91cd498f25fe07c85
Richard Fu <44884321+furic@users.noreply.github.com>
2026-05-02 00:29:08 +1000

    Time: 3 ms (96.15%), Space: 65.7 MB (92.31%) - LeetHub

--- 0396-rotate-function/0396-rotate-function.ts
+++ 0396-rotate-function/0396-rotate-function.ts
@@ -1,13 +1,17 @@
 const maxRotateFunction = (nums: number[]): number => {
     const n = nums.length;
-    const totalSum = nums.reduce((sum, val) => sum + val, 0);
+    let totalSum = 0;
+    let rotatedF = 0;
 
-    // F(k) = F(k-1) + totalSum - n * nums[n-k]
-    let rotatedF = nums.reduce((sum, val, i) => sum + i * val, 0);
+    for (let i = 0; i < n; i++) {
+        totalSum += nums[i];
+        rotatedF += i * nums[i];
+    }
+
     let maxF = rotatedF;
 
     for (let k = 1; k < n; k++) {
-        rotatedF += totalSum - n * nums[n - k];
+        rotatedF += totalSum - n * nums[n - k]; // F(k) = F(k-1) + totalSum - n * nums[n-k]
         maxF = Math.max(maxF, rotatedF);
     }
 

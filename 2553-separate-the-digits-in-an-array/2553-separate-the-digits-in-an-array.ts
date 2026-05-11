4c5ff2d957d1cf560ca7a136a19fadbfee2997b4
Richard Fu <44884321+furic@users.noreply.github.com>
2026-05-11 21:09:03 +1000

    Time: 2 ms (100%), Space: 60.8 MB (42.86%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.ts
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.ts
@@ -1,15 +1,2 @@
-function separateDigits(nums: number[]): number[] {
-    const res: number[] = [];
-    for (const num of nums) {
-        if (num > 9) {
-            const s: string = num.toString();
-            for (const ch of s) {
-                res.push(Number(ch));
-            }
-        } 
-        else {
-            res.push(num);
-        }
-    }
-    return res;
-};
+const separateDigits = (nums: number[]): number[] =>
+    nums.flatMap(n => n.toString().split('').map(Number));

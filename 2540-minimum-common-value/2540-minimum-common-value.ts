ef9496d52562c766aaf7a8aa05389b6851470e8e
Richard Fu <44884321+furic@users.noreply.github.com>
2026-05-19 23:24:20 +1000

    Time: 2 ms (70.97%), Space: 63.2 MB (91.94%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.ts
+++ 2540-minimum-common-value/2540-minimum-common-value.ts
@@ -1,22 +1,11 @@
-function getCommon(nums1: number[], nums2: number[]): number {
-
-    let p1 = 0;
-    let p2 = 0;
+const getCommon = (nums1: number[], nums2: number[]): number => {
+    let p1 = 0, p2 = 0;
 
     while (p1 < nums1.length && p2 < nums2.length) {
-
-        if (nums1[p1] === nums2[p2]) {
-            return nums1[p1];
-        }
-
-        else if (nums1[p1] < nums2[p2]) {
-            p1++;
-        }
-
-        else {
-            p2++;
-        }
+        if      (nums1[p1] === nums2[p2]) return nums1[p1];
+        else if (nums1[p1] <   nums2[p2]) p1++;
+        else                              p2++;
     }
 
     return -1;
-}
+};

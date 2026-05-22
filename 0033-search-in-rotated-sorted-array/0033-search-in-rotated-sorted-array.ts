c5d6de2804753c7c1b0e7618652f2d0030d9a3f9
Richard Fu <44884321+furic@users.noreply.github.com>
2026-05-22 17:37:52 +1000

    Time: 0 ms (100%), Space: 55.7 MB (48.21%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.ts
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.ts
@@ -1,23 +1,21 @@
-function search(nums: number[], target: number): number {
-    let low = 0;
-    let high = nums.length - 1;
-    while (low <= high) {
-        const mid = low + Math.floor((high - low) / 2);
-        if (nums[mid] === target)
-            return mid;
-        if (nums[low] <= nums[mid]) {
-            if (nums[low] <= target && target < nums[mid]) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
+const search = (nums: number[], target: number): number => {
+    let lo = 0;
+    let hi = nums.length - 1;
+
+    while (lo <= hi) {
+        const mid = (lo + hi) >> 1;
+        if (nums[mid] === target) return mid;
+
+        if (nums[lo] <= nums[mid]) {
+            // Left half is sorted
+            if (nums[lo] <= target && target < nums[mid]) hi = mid - 1;
+            else                                          lo = mid + 1;
         } else {
-            if (nums[mid] < target && target <= nums[high]) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+            // Right half is sorted
+            if (nums[mid] < target && target <= nums[hi]) lo = mid + 1;
+            else                                          hi = mid - 1;
         }
     }
+
     return -1;
 };

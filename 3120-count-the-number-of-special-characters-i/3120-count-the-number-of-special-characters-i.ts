87d294d5747c459f137967be8a0e0f54bd83ee5c
Richard Fu <44884321+furic@users.noreply.github.com>
2026-05-26 15:24:08 +1000

    Time: 2 ms (75%), Space: 57 MB (87.5%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.ts
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.ts
@@ -1,8 +1,2 @@
-function numberOfSpecialChars(word: string): number {
-    const set = new Set();
-    for(const c of word){
-        if(word.includes(c.toUpperCase()) && word.includes(c.toLowerCase()))
-            set.add(c);
-    }
-    return set.size / 2;
-};
+const numberOfSpecialChars = (word: string): number =>
+    new Set([...word].filter(c => word.includes(c.toUpperCase()) && word.includes(c.toLowerCase()))).size / 2;

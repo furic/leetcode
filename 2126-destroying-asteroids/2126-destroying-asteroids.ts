74d9b1bdb836f747c26e7fc078659fab45c89e28
Richard Fu <44884321+furic@users.noreply.github.com>
2026-05-31 20:30:01 +1000

    Time: 98 ms (8.33%), Space: 75.5 MB (8.33%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.ts
+++ 2126-destroying-asteroids/2126-destroying-asteroids.ts
@@ -1,10 +1,10 @@
-function asteroidsDestroyed(mass: number, asteroids: number[]): boolean {
+const asteroidsDestroyed = (mass: number, asteroids: number[]): boolean => {
     asteroids.sort((a, b) => a - b);
-    let curr: number = mass;
-    for (const x of asteroids) {
-        if (curr < x)
-            return false;
-        curr += x;
+
+    for (const asteroid of asteroids) {
+        if (mass < asteroid) return false;
+        mass += asteroid;
     }
+
     return true;
 };

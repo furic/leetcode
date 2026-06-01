31ad54059b3a687566858e159d00c8b4510380ba
Richard Fu <44884321+furic@users.noreply.github.com>
2026-06-01 18:33:37 +1000

    Time: 2 ms (53.33%), Space: 56.4 MB (40%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.ts
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.ts
@@ -1,14 +1,10 @@
-function minimumCost(cost: number[]): number {
-    cost.sort((a, b) => a - b);
-    let total = 0;
-    let take = 0;
-    for (let i = cost.length - 1; i >= 0; i--) {
-        if (take === 2) {
-            take = 0;
-        } else {
-            total += cost[i];
-            take++;
-        }
+const minimumCost = (cost: number[]): number => {
+    cost.sort((a, b) => b - a); // Sort descending: buy expensive ones, skip every 3rd
+
+    let totalCost = 0;
+    for (let i = 0; i < cost.length; i++) {
+        if (i % 3 !== 2) totalCost += cost[i];
     }
-    return total;
+
+    return totalCost;
 };

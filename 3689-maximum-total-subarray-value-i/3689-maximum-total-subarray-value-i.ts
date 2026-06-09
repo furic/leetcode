5b2a81c6c606dd1a3428d5772c95a1ca6259daa3
Richard Fu <44884321+furic@users.noreply.github.com>
2026-06-09 20:33:05 +1000

    Time: 2 ms (100%), Space: 67.7 MB (66.67%) - LeetHub

--- 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.ts
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.ts
@@ -1,2 +1,9 @@
-const maxTotalValue = (nums: number[], k: number): number =>
-    (Math.max(...nums) - Math.min(...nums)) * k;
+function maxTotalValue(nums: number[], k: number): number {
+    let min = nums[0],
+        max = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] < min) min = nums[i];
+        else if (nums[i] > max) max = nums[i];
+    }
+    return (max - min) * k;
+}

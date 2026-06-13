afa5eabe8173706f38ea12ce8be924db74ba8095
Richard Fu <44884321+furic@users.noreply.github.com>
2026-06-13 19:45:10 +1000

    Time: 8 ms (11.11%), Space: 61.1 MB (11.11%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.ts
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.ts
@@ -1,11 +1,21 @@
-const mapWordWeights = (words: string[], weights: number[]): string => {
-    let result = "";
-    for (let i = 0; i < words.length; i++) {
-        let weight = 0;
-        for (let j = 0; j < words[i].length; j++) {
-            weight += weights[words[i][j].charCodeAt(0) - 'a'.charCodeAt(0)];
-        }
-        result += String.fromCharCode('z'.charCodeAt(0) - (weight % 26));
+function mapWordWeights(words: string[], weights: number[]): string {
+    const d: Record<string, number> = {};
+    const alpha = "abcdefghijklmnopqrstuvwxyz";
+    for (let i = 0; i < 26; i++) {
+        d[alpha[i]] = weights[i];
     }
-    return result;
-}
+    const cnt: number[] = [];
+    for (const word of words) {
+        let add = 0;
+        for (const x of word) {
+            add += d[x];
+        }
+        const res = add % 26;
+        cnt.push(res);
+    }
+    let ans = "";
+    for (const i of cnt) {
+        ans += String.fromCharCode(96 + 26 - i);
+    }
+    return ans;
+};

a181757ad278c13e8c34e69a6bbe8bae704b4a84
Richard Fu <44884321+furic@users.noreply.github.com>
2026-06-19 00:28:37 +1000

    Time: 0 ms (100%), Space: 55.5 MB (20%) - LeetHub

--- 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.ts
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.ts
@@ -1,4 +1,6 @@
-function angleClock(hour: number, minutes: number) {
-    const d = Math.abs(((hour % 12) + minutes / 60) / 12 - minutes / 60) * 360;
-    return Math.min(d, 360 - d);
-}
+const angleClock = (hour: number, minutes: number): number => {
+    const hourAngle   = ((hour % 12) + minutes / 60) / 12 * 360;
+    const minuteAngle = minutes / 60 * 360;
+    const diff = Math.abs(hourAngle - minuteAngle);
+    return Math.min(diff, 360 - diff);
+};

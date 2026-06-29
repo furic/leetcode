38d81004f88a781f5195bb7ab3c0fc85adecc203
Richard Fu <44884321+furic@users.noreply.github.com>
2026-06-29 18:35:42 +1000

    Time: 0 ms (100%), Space: 55.8 MB (46.43%) - LeetHub

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.ts
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.ts
@@ -1,17 +1,2 @@
-function numOfStrings(patterns: string[], word: string): number {
-    let count: number = 0;
-    for (let pat of patterns) {
-        let left: number = 0;
-        let right: number = pat.length;
-
-        while (right <= word.length) {
-            if (pat == word.slice(left, right)) {
-                count += 1;
-                break;
-            }
-            left += 1;
-            right += 1;
-        }
-    }
-    return count
-};
+const numOfStrings = (patterns: string[], word: string): number =>
+    patterns.filter(pat => word.includes(pat)).length;

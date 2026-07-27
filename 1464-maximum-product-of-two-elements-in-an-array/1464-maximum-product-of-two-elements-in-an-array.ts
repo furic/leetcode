90f71ae58efff70f269e67c006683047746bc4f4
Richard Fu <44884321+furic@users.noreply.github.com>
2026-07-27 23:53:14 +1000

    Time: 0 ms (100%), Space: 58.2 MB (6.45%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.ts
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.ts
@@ -1,13 +1,10 @@
-function maxProduct(nums: number[]): number {
-    let max1 = 0;
-    let max2 = 0;
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] > max1) {
-            max2 = max1;
-            max1 = nums[i];
-        } else if (nums[i] > max2) {
-            max2 = nums[i];
-        }
+const maxProduct = (nums: number[]): number => {
+    let max1 = 0, max2 = 0;
+
+    for (const num of nums) {
+        if      (num > max1) { max2 = max1; max1 = num; }
+        else if (num > max2) { max2 = num; }
     }
+
     return (max1 - 1) * (max2 - 1);
-}
+};

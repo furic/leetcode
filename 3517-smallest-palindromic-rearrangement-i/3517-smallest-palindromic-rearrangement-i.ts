60a6d1d329a8a7aa807f7bedb12e6158f59d196c
Richard Fu <44884321+furic@users.noreply.github.com>
2026-07-28 22:42:12 +1000

    Time: 9 ms (93.33%), Space: 68 MB (66.67%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.ts
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.ts
@@ -3,10 +3,15 @@ const smallestPalindrome = (s: string): string => {
     const freq = new Int32Array(26);
     for (let i = 0; i < halfLen; i++) freq[s.charCodeAt(i) - 97]++;
 
-    let half = '';
-    for (let i = 0; i < 26; i++)
-        if (freq[i] > 0) half += String.fromCharCode(i + 97).repeat(freq[i]);
+    let left = '', right = '';
+    for (let i = 0; i < 26; i++) {
+        if (freq[i] > 0) {
+            const chars = String.fromCharCode(i + 97).repeat(freq[i]);
+            left  += chars;
+            right  = chars + right;
+        }
+    }
 
     const mid = s.length % 2 !== 0 ? s[halfLen] : '';
-    return half + mid + half.split('').reverse().join('');
+    return left + mid + right;
 };

1a30d482de65b4a058ade9e0c1de3eb34ba4a7a2
Richard Fu <44884321+furic@users.noreply.github.com>
2026-08-08 19:04:07 +1000

    Time: 65 ms (80%), Space: 100.5 MB (60%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.ts
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.ts
@@ -1,37 +1,29 @@
-function validSequence(word1: string, word2: string): number[] {
-    const n2 = word2.length;
+const validSequence = (word1: string, word2: string): number[] => {
+    const n = word1.length, m = word2.length;
 
-    const dp2: number[] = new Array(n2).fill(-1);
-    let j = n2 - 1;
-
-    for (let i = word1.length - 1; i >= 0; i--) {
-        if (j >= 0 && word1[i] === word2[j]) {
-            dp2[j] = i;
-            j--;
-        }
+    // Precompute: for each position j in word2, the earliest index in word1
+    // where word2[j..m-1] can be matched going right-to-left
+    const suffixMatch = new Array(m).fill(-1);
+    let j = m - 1;
+    for (let i = n - 1; i >= 0; i--) {
+        if (j >= 0 && word1[i] === word2[j]) suffixMatch[j--] = i;
     }
 
-    let changed = 0;
+    const result: number[] = [];
+    let skipped = 0;
     j = 0;
 
-    const res: number[] = [];
+    for (let i = 0; i < n; i++) {
+        if (j === m) break;
+        const isMatch = word1[i] === word2[j];
+        const canSkip = skipped === 0 && (j === m - 1 || i < suffixMatch[j + 1]);
 
-    for (let i = 0; i < word1.length; i++) {
-        if (j >= n2) {
-            break;
-        }
-
-        if (word1[i] === word2[j]) {
-            res.push(i);
+        if (isMatch || canSkip) {
+            if (!isMatch) skipped++;
+            result.push(i);
             j++;
-        } else if (changed === 0) {
-            if (j === n2 - 1 || i + 1 <= dp2[j + 1]) {
-                changed = 1;
-                res.push(i);
-                j++;
-            }
         }
     }
 
-    return j === n2 ? res : [];
+    return j === m ? result : [];
 };

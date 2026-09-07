8019a74109ffc3a38eeeff482ae8dfbe3bcd8bf3
Richard Fu <44884321+furic@users.noreply.github.com>
2026-09-08 00:04:00 +1000

    Time: 6 ms (66.67%), Space: 57.6 MB (83.33%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.ts
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.ts
@@ -1,16 +1,14 @@
-function distinctSubseqII(s: string): number {
-    const MOD: number = 1000000007;
+const distinctSubseqII = (s: string): number => {
+    const MOD = 1_000_000_007;
+    const lastCount = new Array(26).fill(0); // last contribution of each letter
+    let total = 0;
 
-    const count: number[] = new Array(26).fill(0);
-    let sum: number = 0;
-
-    for (const c of s) {
-        const total: number = (1 + sum) % MOD;
-        const idx: number = c.charCodeAt(0) - 97;
-
-        sum = (sum + total - count[idx] + MOD) % MOD;
-        count[idx] = total;
+    for (const ch of s) {
+        const idx = ch.charCodeAt(0) - 97;
+        const newContrib = (1 + total) % MOD; // all existing subsequences + empty
+        total = (total + newContrib - lastCount[idx] + MOD) % MOD;
+        lastCount[idx] = newContrib;
     }
 
-    return sum;
+    return total;
 };

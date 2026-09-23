95157a9ae33f3fdf7cc6328154b93b62ead5590a
Richard Fu <44884321+furic@users.noreply.github.com>
2026-09-24 01:08:39 +1000

    Time: 3 ms (95%), Space: 67.4 MB (60%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.ts
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.ts
@@ -1,22 +1,17 @@
-function minOperations(nums: number[], x: number): number {
-    let total = 0;
-    for (let num of nums) total += num;
-    if (total < x) return -1;
-    let target = total - x;
+const minOperations = (nums: number[], x: number): number => {
+    const total = nums.reduce((a, b) => a + b, 0);
+    const target = total - x;
+
+    if (target < 0) return -1;
     if (target === 0) return nums.length;
-    let n = nums.length;
-    let left = 0;
-    let sum = 0;
-    let maxLen = -1;
-    for (let right = 0; right < n; ++right) {
-        sum += nums[right];
-        while (sum > target && left <= right) {
-            sum -= nums[left];
-            ++left;
-        }
-        if (sum === target) {
-            maxLen = Math.max(maxLen, right - left + 1);
-        }
+
+    let left = 0, maxMidLen = -1, windowSum = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+        windowSum += nums[right];
+        while (windowSum > target) windowSum -= nums[left++];
+        if (windowSum === target) maxMidLen = Math.max(maxMidLen, right - left + 1);
     }
-    return maxLen === -1 ? -1 : n - maxLen;
+
+    return maxMidLen >= 0 ? nums.length - maxMidLen : -1;
 };

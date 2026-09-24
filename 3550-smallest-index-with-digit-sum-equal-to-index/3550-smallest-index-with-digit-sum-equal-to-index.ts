5a93515309e4a82649a5f05b4593e8e718d193ed
Richard Fu <44884321+furic@users.noreply.github.com>
2026-09-24 20:03:13 +1000

    Time: 2 ms (40.91%), Space: 57.6 MB (81.82%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.ts
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.ts
@@ -1,13 +1,9 @@
 const smallestIndex = (nums: number[]): number => {
     for (let i = 0; i < nums.length; i++) {
-        if (
-            i ===
-            nums[i]
-                .toString()
-                .split("")
-                .reduce((sum, digit) => sum + Number(digit), 0)
-        )
-            return i;
+        let digitSum = 0;
+        for (let n = nums[i]; n > 0; n = Math.floor(n / 10))
+            digitSum += n % 10;
+        if (digitSum === i) return i;
     }
     return -1;
 };
